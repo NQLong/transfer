86146009ac54882329d59db61471c2d14c4d8ad5
Tong Tat Hung <tathung095@gmail.com>
2021-09-06 20:40:49 +0700

    Edit getUserRoles

--- modules/_default/fwUser/model/fwUser.js
+++ modules/_default/fwUser/model/fwUser.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = app => {
-    // app.model.fwUserRole.foo = () => { };
+    app.model.fwUser.getUserRoles = (pemail, done) => {
+        app.dbConnection.execute('BEGIN :ret:=fw_user_get_user_roles(:pemail); END;',
+            { ret: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.CURSOR }, pemail }, (error, result) => app.dbConnection.fetchRowsFromCursor(error, result, done));
+    };
 };
