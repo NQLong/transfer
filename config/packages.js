04efcf9f74c0cfa2eed8ab339d4341c4cd1c2816
Tong Tat Hung <tathung095@gmail.com>
2021-12-23 15:30:12 +0700

    enable cors

--- config/packages.js
+++ config/packages.js
@@ -15,7 +15,9 @@ module.exports = (app, http, config) => {
 
     // Get information from html forms
     const bodyParser = require('body-parser');
+    const cors = require('cors');
 
+    app.use(cors());
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 100000 }));
 
