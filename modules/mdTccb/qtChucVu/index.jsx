79b5d9fedf191e7ae7cc4984b859eef00a9899c6
Hữu Kiệt <kiet.nguyenbk2804@hcmut.edu.vn>
2022-02-15 22:09:05 +0700

    add code for qtChucVu

--- modules/mdTccb/qtChucVu/index.jsx
+++ modules/mdTccb/qtChucVu/index.jsx
@@ -10,7 +10,7 @@ export default {
     },
     routes: [
         {
-            path: '/user/tccb/qua-trinh/chuc-vu/group_cv/:loaiDoiTuong/:shcc',
+            path: '/user/tccb/qua-trinh/chuc-vu/group/:shcc',
             component: Loadable({ loading: Loading, loader: () => import('./adminGroupPage') })
         },
         {
