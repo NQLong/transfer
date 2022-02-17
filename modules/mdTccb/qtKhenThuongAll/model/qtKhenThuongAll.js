e0a369a8ba6de8dbd13147986954715691a3ffe8
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2022-02-17 21:07:34 +0700

    remove not use: groupPageMa function

--- modules/mdTccb/qtKhenThuongAll/model/qtKhenThuongAll.js
+++ modules/mdTccb/qtKhenThuongAll/model/qtKhenThuongAll.js
@@ -6,10 +6,6 @@ module.exports = app => {
                 { ret: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.CURSOR }, pagenumber: { val: pagenumber, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, pagesize: { val: pagesize, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, loaiDoiTuong, maDoiTuong, fromYear, toYear, totalitem: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER }, pagetotal: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER } }, (error, result) => app.dbConnection.fetchRowsFromCursor(error, result, done));
     };
 
-    app.model.qtKhenThuongAll.getByShcc = (isShcc, done) => {
-        app.dbConnection.execute('BEGIN :ret:=qt_khen_thuong_all_by_shcc(:isShcc); END;',
-        { ret: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.CURSOR }, isShcc}, (error, result) => app.dbConnection.fetchRowsFromCursor(error, result, done));
-    };
     app.model.qtKhenThuongAll.searchPage = (pagenumber, pagesize, loaidoituong, ma, fromYear, toYear, searchterm, done) => {
         app.dbConnection.execute('BEGIN :ret:=qt_khen_thuong_all_search_page(:pagenumber, :pagesize, :loaidoituong, :ma, :fromYear, :toYear, :searchterm, :totalitem, :pagetotal); END;',
             { ret: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.CURSOR }, pagenumber: { val: pagenumber, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, pagesize: { val: pagesize, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, loaidoituong, ma, fromYear, toYear, searchterm, totalitem: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER }, pagetotal: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER } }, (error, result) => app.dbConnection.fetchRowsFromCursor(error, result, done));
--- modules/mdTccb/qtNghienCuuKhoaHoc/model/qtNghienCuuKhoaHoc.js
+++ modules/mdTccb/qtNghienCuuKhoaHoc/model/qtNghienCuuKhoaHoc.js
@@ -15,10 +15,6 @@ module.exports = app => {
         app.dbConnection.execute('BEGIN :ret:=qt_nckh_group_page(:pagenumber, :pagesize, :searchterm, :totalitem, :pagetotal, :mscb, :filter); END;',
             { ret: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.CURSOR }, pagenumber: { val: pagenumber, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, pagesize: { val: pagesize, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, searchterm, totalitem: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER }, pagetotal: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER }, mscb, filter }, (error, result) => app.dbConnection.fetchRowsFromCursor(error, result, done));
     };
-    app.model.qtNghienCuuKhoaHoc.groupPageMa = (pagenumber, pagesize, loaiDoiTuong, searchterm, done) => {
-        app.dbConnection.execute('BEGIN :ret:=qt_nckh_group_page_ma(:pagenumber, :pagesize, :loaiDoiTuong, :searchterm, :totalitem, :pagetotal); END;',
-            { ret: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.CURSOR }, pagenumber: { val: pagenumber, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, pagesize: { val: pagesize, dir: app.oracleDB.BIND_INOUT, type: app.oracleDB.NUMBER }, loaiDoiTuong, searchterm, totalitem: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER }, pagetotal: { dir: app.oracleDB.BIND_OUT, type: app.oracleDB.NUMBER } }, (error, result) => app.dbConnection.fetchRowsFromCursor(error, result, done));
-    };
 
     app.model.qtNghienCuuKhoaHoc.userPage = (staffEmail, done) => {
         app.dbConnection.execute('BEGIN :ret:=qt_nghien_cuu_khoa_hoc_user_page(:staffEmail); END;',
