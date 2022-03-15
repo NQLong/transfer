7b4414c0903930914212b0afc45c7d9756a9bf7a
Hữu Kiệt <kiet.nguyenbk2804@hcmut.edu.vn>
2022-03-15 10:47:36 +0700

    add code for qtSangKiem

---
+++ modules/mdTccb/qtSangKien/adminPage.jsx
@@ -0,0 +1,249 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { AdminModal, AdminPage, FormSelect, FormTextBox, renderTable, TableCell, FormRichTextBox } from 'view/component/AdminPage';
+import Pagination from 'view/component/Pagination';
+import { SelectAdapter_FwCanBo } from '../../mdTccb/tccbCanBo/redux';
+import {
+    getQtSangKienPage, deleteQtSangKienStaff, createQtSangKienStaff,
+    updateQtSangKienStaff, getQtSangKienGroupPage
+}
+    from './redux';
+import { SelectAdapter_DmDonVi } from 'modules/mdDanhMuc/dmDonVi/redux';
+
+class EditModal extends AdminModal {
+    state = {
+        id: null,
+    };
+    multiple = false;
+
+    onShow = (item, multiple = true) => {
+        this.multiple = multiple;
+        let { id, shcc, maSo, tenSangKien } = item ? item : {
+            id: '', shcc: '', maSo: '', tenSangKien: ''
+        };
+
+        this.setState({
+            id
+        }, () => {
+            this.maCanBo.value(shcc);
+            this.maSo.value(maSo);
+            this.tenSangKien.value(tenSangKien);
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        let list_ma = this.maCanBo.value();
+        if (!Array.isArray(list_ma)) {
+            list_ma = [list_ma];
+        }
+        if (list_ma.length == 0) {
+            T.notify('Danh sách cán bộ trống', 'danger');
+            this.maCanBo.focus();
+        } else if (!this.maSo.value()) {
+            T.notify('Mã số sáng kiến trống', 'danger');
+            this.maSo.focus();
+        } else if (!this.tenSangKien.value()) {
+            T.notify('Tên sáng kiến trống', 'danger');
+            this.tenSangKien.focus();
+        } else {
+            list_ma.forEach((ma, index) => {
+                const changes = {
+                    shcc: ma,
+                    maSo: this.maSo.value(),
+                    tenSangKien: this.tenSangKien.value(),
+                };
+                if (index == list_ma.length - 1) {
+                    this.state.id ? this.props.update(this.state.id, changes, this.hide) : this.props.create(changes, this.hide);
+                    this.setState({
+                        id: ''
+                    });
+                    this.maCanBo.reset();
+                }
+                else {
+                    this.state.id ? this.props.update(this.state.id, changes, null) : this.props.create(changes, null);
+                }
+            });
+        }
+    }
+
+    render = () => {
+        const readOnly = this.props.readOnly;
+        return this.renderModal({
+            title: this.state.id ? 'Cập nhật danh sách sáng kiến' : 'Tạo mới danh sách sáng kiến',
+            size: 'large',
+            body: <div className='row'>
+                <FormSelect className='col-md-12' multiple={this.multiple} ref={e => this.maCanBo = e} label='Cán bộ' data={SelectAdapter_FwCanBo} readOnly={this.state.id ? true : false} required />
+                <FormTextBox className='col-md-12' ref={e => this.maSo = e} label='Mã số sáng kiến' readOnly={readOnly} required />
+                <FormRichTextBox className='col-md-12' ref={e => this.tenSangKien = e} label='Tên sáng kiến' readOnly={readOnly} required /> 
+            </div>
+        });
+    }
+}
+
+class QtSangKien extends AdminPage {
+    checked = false;
+    state = { filter: {} };
+    menu = '';
+    componentDidMount() {
+        T.ready('/user/tccb', () => {
+            T.clearSearchBox();
+            T.onSearch = (searchText) => this.getPage(undefined, undefined, searchText || '');
+            T.showSearchBox(() => {
+                this.maDonVi?.value('');
+                this.mulCanBo?.value('');
+                setTimeout(() => this.changeAdvancedSearch(), 50);
+            });
+            this.getPage();
+            this.changeAdvancedSearch(true);
+        });
+    }
+
+    showModal = (e) => {
+        e.preventDefault();
+        this.modal.show();
+    }
+
+    changeAdvancedSearch = (isInitial = false) => {
+        let { pageNumber, pageSize } = this.props && this.props.qtSangKien && this.props.qtSangKien.page ? this.props.qtSangKien.page : { pageNumber: 1, pageSize: 50 };
+        const list_dv = this.maDonVi?.value().toString() || '';
+        const list_shcc = this.mulCanBo?.value().toString() || '';
+        const pageFilter = isInitial ? null : { list_dv, list_shcc };
+        this.setState({ filter: pageFilter }, () => {
+            this.getPage(pageNumber, pageSize, '', (page) => {
+                if (isInitial) {
+                    const filter = page.filter || {};
+                    this.setState({ filter: !$.isEmptyObject(filter) ? filter : pageFilter });
+                    this.maDonVi?.value(filter.list_dv);
+                    this.mulCanBo?.value(filter.list_shcc);
+                    if (!$.isEmptyObject(filter) && filter && (filter.fromYear || filter.toYear || filter.list_shcc || filter.list_dv)) this.showAdvanceSearch();
+                }
+            });
+        });
+    }
+
+    getPage = (pageN, pageS, pageC, done) => {
+        this.props.getQtSangKienPage(pageN, pageS, pageC, this.state.filter, done);
+    }
+
+    groupPage = () => {
+        this.checked = !this.checked;
+        T.cookie('hienThiTheoCanBo', this.checked ? 1 : 0);
+        this.getPage();
+    }
+
+    list = (text, i, j) => {
+        if (!text) return [];
+        let items = text.split('??').map(str => <p key={i--}>{j - i}. {str}</p>);
+        return items;
+    }
+
+    delete = (e, item) => {
+        T.confirm('Xóa danh sách sáng kiến', 'Bạn có chắc bạn muốn xóa danh sách sáng kiến này?', 'warning', true, isConfirm => {
+            isConfirm && this.props.deleteQtSangKienStaff(item.id, error => {
+                if (error) T.notify(error.message ? error.message : 'Xoá danh sách sáng kiến bị lỗi!', 'danger');
+                else T.alert('Xoá danh sách sáng kiến thành công!', 'success', false, 800);
+            });
+        });
+        e.preventDefault();
+    }
+
+    render() {
+        const currentPermissions = this.props.system && this.props.system.user && this.props.system.user.permissions ? this.props.system.user.permissions : [],
+            permission = this.getUserPermission('qtSangKien', ['read', 'write', 'delete']);
+        let { pageNumber, pageSize, pageTotal, totalItem, pageCondition, list } = this.props.qtSangKien && this.props.qtSangKien.page ? this.props.qtSangKien.page
+         : { pageNumber: 1, pageSize: 50, pageTotal: 1, totalItem: 0, pageCondition: {}, list: [] };
+        let table  = renderTable({
+            emptyTable: 'Không có dữ liệu về sáng kiến',
+                getDataSource: () => list, stickyHead: true,
+                renderHead: () => (
+                    <tr>
+                        <th style={{ width: 'auto', textAlign: 'right' }}>#</th>
+                        <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Cán bộ</th>
+                        <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Chức danh</th>
+                        <th style={{ width: '30%', whiteSpace: 'nowrap' }}>Chức vụ</th>
+                        <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Mã số</th>
+                        <th style={{ width: '70%', whiteSpace: 'nowrap' }}>Tên phát minh</th>
+                        <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Thao tác</th>
+                    </tr>
+                ),
+                renderRow: (item, index) => (
+                    <tr key={index}>
+                        <TableCell type='text' style={{ textAlign: 'right' }} content={(pageNumber - 1) * pageSize + index + 1} />
+                        <TableCell type='link' onClick={() => this.modal.show(item, false)} style={{ whiteSpace: 'nowrap' }} content={(
+                            <>
+                                <span>{(item.hoCanBo ? item.hoCanBo : ' ') + ' ' + (item.tenCanBo ? item.tenCanBo : ' ')}</span><br />
+                                {item.shcc}
+                            </>
+                        )}
+                        />
+                        <TableCell type='text' content={(
+                            <>
+                                {item.tenNgach ? item.tenNgach : ''}
+                            </>
+                        )}
+                        />  
+                        <TableCell type='text' content={(
+                            <>
+                                {(item.tenChucVu ? item.tenChucVu : '') + ' ' + (item.tenDonVi ? item.tenDonVi.normalizedName() : (item.tenBoMon ? item.tenBoMon.normalizedName() : ''))}
+                            </>
+                        )}
+                        />  
+                        <TableCell type='text' style={{ whiteSpace: 'nowrap' }} content={(
+                            <>
+                                {item.maSo ? item.maSo : ''}
+                            </>
+                        )}
+                        />  
+                        <TableCell type='text' content={(
+                            <>
+                                {item.tenSangKien ? item.tenSangKien : ''}
+                            </>
+                        )}
+                        />
+                        {
+                            <TableCell type='buttons' style={{ textAlign: 'center' }} content={item} permission={permission}
+                                onEdit={() => this.modal.show(item, false)} onDelete={this.delete} >
+                            </TableCell>
+                        }
+                    </tr>
+                )
+            });
+
+        return this.renderPage({
+            icon: 'fa fa-cogs',
+            title: ' Quá trình sáng kiến',
+            breadcrumb: [
+                <Link key={0} to='/user/tccb'>Tổ chức cán bộ</Link>,
+                'Quá trình sáng kiến'
+            ],
+            advanceSearch: <>
+                <div className='row'>
+                    <FormSelect className='col-12 col-md-6' multiple={true} ref={e => this.maDonVi = e} label='Đơn vị' data={SelectAdapter_DmDonVi} onChange={() => this.changeAdvancedSearch()} allowClear={true} minimumResultsForSearch={-1} />
+                    <FormSelect className='col-12 col-md-6' multiple={true} ref={e => this.mulCanBo = e} label='Cán bộ' data={SelectAdapter_FwCanBo} onChange={() => this.changeAdvancedSearch()} allowClear={true} minimumResultsForSearch={-1} />
+                </div>
+            </>,
+            content: <>
+                <div className='tile'>
+                    {table}
+                </div>
+                <Pagination style={{ marginLeft: '70px' }} {...{ pageNumber, pageSize, pageTotal, totalItem, pageCondition }}
+                    getPage={this.getPage} />
+                <EditModal ref={e => this.modal = e} permission={permission}
+                    create={this.props.createQtSangKienStaff} update={this.props.updateQtSangKienStaff}
+                    permissions={currentPermissions}
+                />
+            </>,
+            backRoute: '/user/' + this.menu,
+            onCreate: permission && permission.write && !this.checked ? (e) => this.showModal(e) : null,
+        });
+    }
+}
+
+const mapStateToProps = state => ({ system: state.system, qtSangKien: state.tccb.qtSangKien });
+const mapActionsToProps = {
+    getQtSangKienPage, deleteQtSangKienStaff, createQtSangKienStaff,
+    updateQtSangKienStaff, getQtSangKienGroupPage,
+};
+export default connect(mapStateToProps, mapActionsToProps)(QtSangKien);
---
+++ modules/mdTccb/qtSangKien/controller.js
@@ -0,0 +1,139 @@
+module.exports = app => {
+    const menu = {
+        parentMenu: app.parentMenu.tccb,
+        menus: {
+            9503: { title: 'Quá trình sáng kiến', link: '/user/tccb/qua-trinh/sang-kien', icon: 'fa fa-cogs', color: '#000000', backgroundColor: '#00e34c', groupIndex: 3 },
+        },
+    };
+    // const menuStaff = {
+    //     parentMenu: app.parentMenu.user,
+    //     menus: {
+    //         1030: { title: 'Danh sách sáng kiến', link: '/user/sang-kien', icon: 'fa fa-cogs', backgroundColor: '#00e34c', groupIndex: 4 },
+    //     },
+    // };
+
+    // const menuTCCB = {
+    //     parentMenu: app.parentMenu.tccb,
+    //     menus: {
+    //         3070: { title: 'Danh sách sáng kiến', link: '/user/tccb/qua-trinh/sang-kien', icon: 'fa fa-cogs', backgroundColor: '#00e34c', groupIndex: 3 },
+    //     },
+    // };
+
+    app.permission.add(
+        // { name: 'staff:login', menu: menuStaff },
+        // { name: 'qtSangKien:read', menu: menuTCCB },
+        { name: 'qtSangKien:read', menu },
+        { name: 'qtSangKien:write' },
+        { name: 'qtSangKien:delete' },
+    );
+    app.get('/user/tccb/qua-trinh/sang-kien', app.permission.check('qtSangKien:read'), app.templates.admin);
+    // app.get('/user/tccb/qua-trinh/sang-kien/group/:shcc', app.permission.check('qtSangKien:read'), app.templates.admin);
+    // app.get('/user/sang-kien', app.permission.check('staff:login'), app.templates.admin);
+
+    // APIs -----------------------------------------------------------------------------------------------------------------------------------------
+    // //User Actions:
+    app.post('/api/user/qua-trinh/sang-kien', app.permission.check('staff:login'), (req, res) => {
+        if (req.body.data && req.session.user) {
+            const data = req.body.data;
+            app.model.qtSangKien.create(data, (error, item) => res.send({ error, item }));
+        } else {
+            res.send({ error: 'Invalid parameter!' });
+        }
+    });
+
+    app.put('/api/user/qua-trinh/sang-kien', app.permission.check('staff:login'), (req, res) => {
+        if (req.body.changes && req.session.user) {
+            app.model.qtSangKien.get({ id: req.body.id }, (error, item) => {
+                if (error || item == null) {
+                    res.send({ error: 'Not found!' });
+                } else {
+                    app.model.canBo.get({ shcc: item.shcc }, (e, r) => {
+                        if (e || r == null) res.send({ error: 'Staff not found!' }); else {
+                            const changes = req.body.changes;
+                            app.model.qtSangKien.update({ id: req.body.id }, changes, (error, item) => res.send({ error, item }));
+                        }
+                    });
+                }
+            });
+        } else {
+            res.send({ error: 'Invalid parameter!' });
+        }
+    });
+
+    app.delete('/api/user/qua-trinh/sang-kien', app.permission.check('staff:login'), (req, res) => {
+        if (req.session.user) {
+            app.model.qtSangKien.get({ id: req.body.id }, (error, item) => {
+                if (error || item == null) {
+                    res.send({ error: 'Not found!' });
+                } else {
+                    app.model.canBo.get({ shcc: item.shcc }, (e, r) => {
+                        if (e || r == null) res.send({ error: 'Staff not found!' }); else {
+                            app.model.qtSangKien.delete({ id: req.body.id }, (error, item) => res.send({ error, item }));
+                        }
+                    });
+                }
+            });
+        } else {
+            res.send({ error: 'Invalid parameter!' });
+        }
+    });
+
+    app.get('/api/user/qua-trinh/sang-kien/page/:pageNumber/:pageSize', app.permission.check('staff:login'), (req, res) => {
+        const pageNumber = parseInt(req.params.pageNumber),
+            pageSize = parseInt(req.params.pageSize),
+            searchTerm = typeof req.query.condition === 'string' ? req.query.condition : '';
+        const { list_shcc, list_dv } = (req.query.filter && req.query.filter != '%%%%%%%%') ? req.query.filter : { list_shcc: null, list_dv: null };
+        app.model.qtSangKien.searchPage(pageNumber, pageSize, list_shcc, list_dv, searchTerm, (error, page) => {
+            if (error || page == null) {
+                res.send({ error });
+            } else {
+                const { totalitem: totalItem, pagesize: pageSize, pagetotal: pageTotal, pagenumber: pageNumber, rows: list } = page;
+                const pageCondition = searchTerm;
+                res.send({ error, page: { totalItem, pageSize, pageTotal, pageNumber, pageCondition, list } });
+            }
+        });
+    });
+    ///END USER ACTIONS
+    
+    app.get('/api/tccb/qua-trinh/sang-kien/page/:pageNumber/:pageSize', app.permission.check('qtSangKien:read'), (req, res) => {
+        const pageNumber = parseInt(req.params.pageNumber),
+            pageSize = parseInt(req.params.pageSize),
+            searchTerm = typeof req.query.condition === 'string' ? req.query.condition : '';
+        const { list_shcc, list_dv } = (req.query.filter && req.query.filter != '%%%%%%%%%%') ? req.query.filter : { list_shcc: null, list_dv: null };
+        app.model.qtSangKien.searchPage(pageNumber, pageSize, list_shcc, list_dv, searchTerm, (error, page) => {
+            if (error || page == null) {
+                res.send({ error });
+            } else {
+                const { totalitem: totalItem, pagesize: pageSize, pagetotal: pageTotal, pagenumber: pageNumber, rows: list } = page;
+                const pageCondition = searchTerm;
+                res.send({ error, page: { totalItem, pageSize, pageTotal, pageNumber, pageCondition, list } });
+            }
+        });
+    });
+
+    app.get('/api/tccb/qua-trinh/sang-kien/group/page/:pageNumber/:pageSize', app.permission.check('qtSangKien:read'), (req, res) => {
+        const pageNumber = parseInt(req.params.pageNumber),
+            pageSize = parseInt(req.params.pageSize),
+            searchTerm = typeof req.query.condition === 'string' ? req.query.condition : '';
+        const { list_shcc, list_dv } = (req.query.filter && req.query.filter != '%%%%%%%%%%') ? req.query.filter : { list_shcc: null, list_dv: null };
+        app.model.qtSangKien.groupPage(pageNumber, pageSize, list_shcc, list_dv, searchTerm, (error, page) => {
+            if (error || page == null) {
+                res.send({ error });
+            } else {
+                const { totalitem: totalItem, pagesize: pageSize, pagetotal: pageTotal, pagenumber: pageNumber, rows: list } = page;
+                const pageCondition = searchTerm;
+                res.send({ error, page: { totalItem, pageSize, pageTotal, pageNumber, pageCondition, list } });
+            }
+        });
+    });
+    
+    app.post('/api/qua-trinh/sang-kien', app.permission.check('qtSangKien:write'), (req, res) => 
+        app.model.qtSangKien.create(req.body.data, (error, item) => res.send({ error, item })));
+
+    app.put('/api/qua-trinh/sang-kien', app.permission.check('qtSangKien:write'), (req, res) =>
+        app.model.qtSangKien.update({ id: req.body.id }, req.body.changes, (error, item) => res.send({ error, item })));
+
+    app.delete('/api/qua-trinh/sang-kien', app.permission.check('qtSangKien:write'), (req, res) =>
+        app.model.qtSangKien.delete({ id: req.body.id }, (error) => res.send(error)));
+
+};
