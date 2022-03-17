66a7c6849aee3cea48acd3d5fe3c40d4abd6ea06
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2022-03-17 09:17:51 +0700

    add 1 cases in output

--- asset/tool/app.tool.testFileCanBo.js
+++ asset/tool/app.tool.testFileCanBo.js
@@ -24,53 +24,79 @@ const run = () => {
     app.excel.readFile(app.path.join(app.assetPath, './data/DSCB.xlsx'), workbook => {
         if (workbook) {
             const worksheet = workbook.getWorksheet(1);
-            solve = (index = 1) => {
-                let number = worksheet.getCell('A' + index).value;
-                if (number == null) {
-                    process.exit(1);
-                }
-                let shcc = worksheet.getCell('B' + index).value;
-                let ho = worksheet.getCell('C' + index).value;
-                if (ho) {
-                    ho = ho.toString().trim();
-                }
-                let ten = worksheet.getCell('D' + index).value;
-                if (ten) {
-                    ten = ten.toString().trim();
-                }
-                let ngaySinh = worksheet.getCell('E' + index).value;
-                if (ngaySinh) {
-                    ngaySinh = ngaySinh.toString().trim();
-                }
-                let donVi = worksheet.getCell('L' + index).value;
-                if (donVi) {
-                    donVi = donVi.toString().trim();
-                }
-                app.model.dmDonVi.getMaDonVi(donVi, (maDv, tenDv) => {
-                    app.model.canBo.getShccCanBo({ho: ho, ten: ten, ngaySinh: ngaySinh, maDonVi: maDv }, (error, shccAns) => {
-                        if (error || shccAns == null) {
-                            //console.log(shcc + ',' + ho + ' ' + ten + ',' + donVi);
+            app.model.canBo.getAll((error, itemsCB) => {
+                for (let i = 0; i < itemsCB.length; i++) {
+                    let has = false;
+                    for (let j = 1; ; j++) {
+                        let number = worksheet.getCell('A' + j).value;
+                        if (number == null) {
+                            break;
                         }
-                        else {
-                            if (shcc != shccAns) {
-                                app.model.canBo.get({shcc: shcc}, (error, item) => {
-                                    if (error || item == null) {
-                                        app.model.canBo.get({shcc: shccAns}, (error, item2) => {
-                                            app.model.dmDonVi.get({ma: item2.maDonVi}, (error, itemDv) => {
-                                                let nameDv = '';
-                                                if (itemDv != null) nameDv = itemDv.ten;
-                                                console.log('Trong file excel: ' + shcc + ' - ' + ho + ' ' + ten + ' - ' + donVi + '\nTrong cơ sở dữ liệu: ' + item2.shcc + ' - ' + item2.ho + ' ' + item2.ten + ' - ' + nameDv + '\n');
-                                            });
-                                        });
-                                    }
-                                })
+                        let shcc = worksheet.getCell('B' + j).value;
+                        if (shcc) {
+                            shcc = shcc.toString().trim();
+                            if (itemsCB[i].shcc == shcc) {
+                                has = true;
+                                break;
                             }
                         }
-                        solve(index + 1);
-                    });
-                });
-            }
-            if (worksheet) solve();
+                    }
+                    if (!has) {
+                        app.model.dmDonVi.get({ma: itemsCB[i].maDonVi}, (error, itemDv) => {
+                            let nameDv = '';
+                            if (itemDv != null) nameDv = itemDv.ten;
+                            console.log(itemsCB[i].shcc + ',' + itemsCB[i].ho + ' ' + itemsCB[i].ten + ',' + nameDv);
+                        });
+                    }
+                }
+            });
+            // solve = (index = 1) => {
+            //     let number = worksheet.getCell('A' + index).value;
+            //     if (number == null) {
+            //         process.exit(1);
+            //     }
+            //     let shcc = worksheet.getCell('B' + index).value;
+            //     let ho = worksheet.getCell('C' + index).value;
+            //     if (ho) {
+            //         ho = ho.toString().trim();
+            //     }
+            //     let ten = worksheet.getCell('D' + index).value;
+            //     if (ten) {
+            //         ten = ten.toString().trim();
+            //     }
+            //     let ngaySinh = worksheet.getCell('E' + index).value;
+            //     if (ngaySinh) {
+            //         ngaySinh = ngaySinh.toString().trim();
+            //     }
+            //     let donVi = worksheet.getCell('L' + index).value;
+            //     if (donVi) {
+            //         donVi = donVi.toString().trim();
+            //     }
+            //     app.model.dmDonVi.getMaDonVi(donVi, (maDv, tenDv) => {
+            //         app.model.canBo.getShccCanBo({ho: ho, ten: ten, ngaySinh: ngaySinh, maDonVi: maDv }, (error, shccAns) => {
+            //             if (error || shccAns == null) {
+            //                 //console.log(shcc + ',' + ho + ' ' + ten + ',' + donVi);
+            //             }
+            //             else {
+            //                 if (shcc != shccAns) {
+            //                     app.model.canBo.get({shcc: shcc}, (error, item) => {
+            //                         if (error || item == null) {
+            //                             app.model.canBo.get({shcc: shccAns}, (error, item2) => {
+            //                                 app.model.dmDonVi.get({ma: item2.maDonVi}, (error, itemDv) => {
+            //                                     let nameDv = '';
+            //                                     if (itemDv != null) nameDv = itemDv.ten;
+            //                                     console.log('Trong file excel: ' + shcc + ' - ' + ho + ' ' + ten + ' - ' + donVi + '\nTrong cơ sở dữ liệu: ' + item2.shcc + ' - ' + item2.ho + ' ' + item2.ten + ' - ' + nameDv + '\n');
+            //                                 });
+            //                             });
+            //                         }
+            //                     })
+            //                 }
+            //             }
+            //             solve(index + 1);
+            //         });
+            //     });
+            // }
+            // if (worksheet) solve();
         }
     });
 };
