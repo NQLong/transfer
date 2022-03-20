d2958fba0a7de04a84498f1c5b49b13b3456daae
Long Nguyen <nqlong0709@gmail.com>
2022-03-20 16:31:32 +0700

    merge fix

--- modules/mdHanhChinhTongHop/hcthCongVanDen/adminPage.jsx
+++ modules/mdHanhChinhTongHop/hcthCongVanDen/adminPage.jsx
@@ -10,15 +10,14 @@ import T from 'view/js/common';
 import Pagination from 'view/component/Pagination';
 
 class EditModal extends AdminModal {
-    state = {};
+    state = { id: null };
     componentDidMount() {
-        $(document).ready(() => this.onShown(() => { }));
+        T.ready(() => this.onShown(() => { this.soCV.focus(); }));
     }
 
     onShow = (item) => {
         let { id, ngayCongVan, ngayNhan, ngayHetHan, soCongVan, maDonViGuiCV, maDonVi, shcc, noiDung, chiDao } = item ? item : { id: '', ngayCV: '', ngayNhan: '', ngayHetHan: '', soCongVan: '', maDonViGuiCV: '', maDonVi: '', shcc: '', noiDung: '', chiDao: '' };
-        // console.log(donViGui)
-        this.setState({ id, item });
+        this.setState({ id });
         this.ngayCV.value(ngayCongVan);
         this.ngayNhan.value(ngayNhan);
         this.ngayHetHan.value(ngayHetHan);
@@ -44,7 +43,6 @@ class EditModal extends AdminModal {
             noiDung: this.noiDung.value(),
             chiDao: this.chiDao.value()
         };
-        console.log(changes);
         if (!changes.ngayCongVan) {
             T.notify('Ngày công văn bị trống', 'danger');
             this.ngayCV.focus();
@@ -58,14 +56,14 @@ class EditModal extends AdminModal {
             T.notify('Nội dung công văn bị trống', 'danger');
             this.noiDung.focus();
         } else {
-            this.state.id ? this.props.update(this.state.id, changes, this.hide) : this.props.create(changes, this.hide);
+            (this.state.id && this.props.permission.write) ? this.props.update(this.state.id, changes, this.hide) : this.props.create(changes, this.hide);
         }
     }
 
     render = () => {
         const readOnly = this.props.readOnly;
         return this.renderModal({
-            title: typeof this.state.item == 'object' ? 'Cập nhật Công văn đến' : 'Tạo mới Công văn đến',
+            title: this.state.id ? 'Cập nhật Công văn đến' : 'Tạo mới Công văn đến',
             size: 'large',
             body: (
                 <div className='form-group row'>
@@ -110,7 +108,7 @@ class HcthCongVanDen extends AdminPage {
         const donViGuiCongVan = this.donViGuiCongVan?.value();
         const donViNhanCongVan = this.donViNhanCongVan?.value();
         const canBoNhanCongVan = this.canBoNhanCongVan?.value();
-        const pageFilter = isInitial ? {}: {donViGuiCongVan, donViNhanCongVan, canBoNhanCongVan};
+        const pageFilter = isInitial ? {} : { donViGuiCongVan, donViNhanCongVan, canBoNhanCongVan };
         this.setState({ filter: pageFilter }, () => {
             this.getPage(pageNumber, pageSize, '', (page) => {
                 if (isInitial) {
@@ -142,65 +140,56 @@ class HcthCongVanDen extends AdminPage {
     render() {
         const currentPermissions = this.props.system && this.props.system.user && this.props.system.user.permissions ? this.props.system.user.permissions : [],
             permission = this.getUserPermission('hcthCongVanDen', ['read', 'write', 'delete']);
+        let readOnly = !permission.write;
         let { pageNumber, pageSize, pageTotal, totalItem, pageCondition, list } = this.props.hcthCongVanDen ? this.props.hcthCongVanDen.page : { pageNumber: 1, pageSize: 50, pageTotal: 1, totalItem: 0, pageCondition: {}, list: [] };
-        let table = 'Không có danh sách!';
-        if (list && list.length > 0) {
-            table = renderTable({
-                getDataSource: () => list,
-                stickyHead: false,
-                renderHead: () => (
-                    <tr>
-                        <th style={{ width: 'auto', textAlign: 'center' }}>#</th>
-                        <th style={{ width: '30%', whiteSpace: 'nowrap', textAlign: 'center' }}>Số CV</th>
-                        <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Ngày CV</th>
-                        <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Ngày nhận</th>
-                        <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Ngày hết hạn</th>
-                        <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Đơn vị gửi</th>
-                        <th style={{ width: '50%', whiteSpace: 'nowrap', textAlign: 'center' }}>Nội dung</th>
-                        <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Đơn vị nhận</th>
-                        <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Cán bộ nhận</th>
-                        <th style={{ width: '20%' }}>Chỉ đạo</th>
-                        <th style={{ width: 'auto', textAlign: 'center', whiteSpace: 'nowrap' }}>Thao tác</th>
-                    </tr>),
-                renderRow: (item, index) => (
-                    <tr key={index}>
-                        <TableCell type='text' style={{ textAlign: 'right' }} content={(pageNumber - 1) * pageSize + index + 1} />
-                        <TableCell type='link' onClick={() => this.modal.show(item)} style={{ textAlign: 'center', whiteSpace: 'nowrap' }} content={item.soCongVan} />
-                        <TableCell type='text' style={{ textAlign: 'center', whiteSpace: 'nowrap' }} content={T.dateToText(item.ngayCongVan, 'dd/mm/yyyy')} />
-                        <TableCell type='text' style={{ textAlign: 'center', whiteSpace: 'nowrap' }} content={T.dateToText(item.ngayNhan, 'dd/mm/yyyy')} />
-                        <TableCell type='text' style={{ textAlign: 'center', whiteSpace: 'nowrap', color: 'red' }} content={item.ngayHetHan ? T.dateToText(item.ngayHetHan, 'dd/mm/yyyy') : ''} />
-                        <TableCell type='text' style={{ textAlign: 'center' }} content={item.tenDonViGuiCV} />
-                        <TableCell type='text' style={{}} content={item.noiDung} />
-                        <TableCell type='text' style={{ textAlign: 'center' }} content={item.tenDonVi?.normalizedName()} />
-                        <TableCell type='text' onClick={() => this.modal.show(item)} style={{}} content={(
-                            item.shcc &&
-                            <>
-                                <span>{((item.hoCanBo ? item.hoCanBo : ' ') + ' ' + (item.tenCanBo ? item.tenCanBo : ' ')).normalizedName()}</span><br />
-                                {item.tenDonViCanBo?.normalizedName()}
-                            </>
-                        )}
-                        />
-                        <TableCell type='text' style={{}} content={item.chiDao} />
-                        <TableCell type='buttons' style={{ textAlign: 'center' }} content={item} permission={permission} onEdit={() => this.modal.show(item)} onDelete={() => this.onDelete(item.id)} permissions={currentPermissions} />
+        let table = renderTable({
+            getDataSource: () => list,
+            emptyTable: 'Không dữ liệu công văn đến',
+            stickyHead: false,
+            renderHead: () => (
+                <tr>
+                    <th style={{ width: 'auto', textAlign: 'center' }}>#</th>
+                    <th style={{ width: '30%', whiteSpace: 'nowrap', textAlign: 'center' }}>Số CV</th>
+                    <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Ngày CV</th>
+                    <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Ngày nhận</th>
+                    <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Ngày hết hạn</th>
+                    <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Đơn vị gửi</th>
+                    <th style={{ width: '50%', whiteSpace: 'nowrap', textAlign: 'center' }}>Nội dung</th>
+                    <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Đơn vị nhận</th>
+                    <th style={{ width: 'auto', whiteSpace: 'nowrap', textAlign: 'center' }}>Cán bộ nhận</th>
+                    <th style={{ width: '20%' }}>Chỉ đạo</th>
+                    <th style={{ width: 'auto', textAlign: 'center', whiteSpace: 'nowrap' }}>Thao tác</th>
+                </tr>),
+            renderRow: (item, index) => (
+                <tr key={index}>
+                    <TableCell type='text' style={{ textAlign: 'right' }} content={(pageNumber - 1) * pageSize + index + 1} />
+                    <TableCell type='link' onClick={() => this.modal.show(item)} style={{ textAlign: 'center', whiteSpace: 'nowrap' }} content={item.soCongVan} />
+                    <TableCell type='text' style={{ textAlign: 'center', whiteSpace: 'nowrap' }} content={T.dateToText(item.ngayCongVan, 'dd/mm/yyyy')} />
+                    <TableCell type='text' style={{ textAlign: 'center', whiteSpace: 'nowrap' }} content={T.dateToText(item.ngayNhan, 'dd/mm/yyyy')} />
+                    <TableCell type='text' style={{ textAlign: 'center', whiteSpace: 'nowrap', color: 'red' }} content={item.ngayHetHan ? T.dateToText(item.ngayHetHan, 'dd/mm/yyyy') : ''} />
+                    <TableCell type='text' style={{ textAlign: 'center' }} content={item.tenDonViGuiCV} />
+                    <TableCell type='text' style={{}} content={item.noiDung} />
+                    <TableCell type='text' style={{ textAlign: 'center' }} content={item.tenDonVi?.normalizedName()} />
+                    <TableCell type='text' onClick={() => this.modal.show(item)} style={{}} content={(
+                        item.shcc &&
+                        <>
+                            <span>{((item.hoCanBo ? item.hoCanBo : ' ') + ' ' + (item.tenCanBo ? item.tenCanBo : ' ')).normalizedName()}</span><br />
+                            {item.tenDonViCanBo?.normalizedName()}
+                        </>
+                    )}
+                    />
+                    <TableCell type='text' style={{}} content={item.chiDao} />
+                    <TableCell type='buttons' style={{ textAlign: 'center' }} content={item} permission={permission} onEdit={() => this.modal.show(item)} onDelete={() => this.onDelete(item.id)} permissions={currentPermissions} />
 
-                    </tr>)
-            });
-        }
+                </tr>)
+        });
         return this.renderPage({
             icon: 'fa fa-caret-square-o-left',
             title: 'Công văn đến',
             breadcrumb: [
                 <Link key={0} to='/user/hcth'>Hành chính tổng hợp</Link>,
-                'Văn bản đến'
+                'Công văn đến'
             ],
-            onCreate: permission && permission.write ? (e) => this.showModal(e) : null,
-            backRoute: '/user/hcth',
-            content: <div className='tile'>
-                {table}
-                <Pagination style={{ marginLeft: '70px' }} {...{ pageNumber, pageSize, pageTotal, totalItem, pageCondition }}
-                    getPage={this.getPage} />
-                <EditModal ref={e => this.modal = e} readOnly={false} create={this.props.createHcthCongVanDen} update={this.props.updateHcthCongVanDen} />
-            </div>,
             advanceSearch: <>
                 <div className='row'>
                     <FormSelect allowClear={true} className='col-md-4' ref={e => this.donViGuiCongVan = e} label='Đơn vị gửi công văn' data={SelectAdapter_DmDonViGuiCongVan} onChange={() => this.changeAdvancedSearch()} />
@@ -208,6 +197,15 @@ class HcthCongVanDen extends AdminPage {
                     <FormSelect allowClear={true} className='col-md-4' ref={e => this.canBoNhanCongVan = e} label='Cán bộ nhận công văn' data={SelectAdapter_FwCanBo} onChange={() => this.changeAdvancedSearch()} />
                 </div>
             </>,
+            content: <div className='tile'>
+                {table}
+                <Pagination style={{ marginLeft: '70px' }} {...{ pageNumber, pageSize, pageTotal, totalItem, pageCondition }}
+                    getPage={this.getPage} />
+                <EditModal ref={e => this.modal = e} readOnly={readOnly} create={this.props.createHcthCongVanDen} update={this.props.updateHcthCongVanDen} permission={permission} />
+            </div>,
+
+            onCreate: permission && permission.write ? (e) => this.showModal(e) : null,
+            backRoute: '/user/hcth',
         });
     }
 }
