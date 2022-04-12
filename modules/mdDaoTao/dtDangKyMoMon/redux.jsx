e09e534bcd938f4de770bbe6833bc3a2d71b0c86
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2022-04-12 08:58:24 +0700

    tab size

--- modules/mdDaoTao/dtDangKyMoMon/redux.jsx
+++ modules/mdDaoTao/dtDangKyMoMon/redux.jsx
@@ -7,56 +7,57 @@ const DtDangKyMoMonUpdate = 'DtDangKyMoMon:Update';
 // const DtDangKyMoMonGetItem = 'DtDangKyMoMon:GetItem';
 
 export default function dtDangKyMoMonReducer(state = null, data) {
-     switch (data.type) {
-          case DtDangKyMoMonGetPage:
-               return Object.assign({}, state, { page: data.page });
-          // case DtDangKyMoMonGetItem:
-          //     return Object.assign({}, state, { item: data.item });
-          case DtDangKyMoMonUpdate:
-               if (state) {
-                    let updatedItems = Object.assign({}, state.items),
-                         updatedPage = Object.assign({}, state.page),
-                         updatedItem = data.item;
-                    if (updatedItems) {
-                         for (let i = 0, n = updatedItems.length; i < n; i++) {
-                              if (updatedItems[i].id == updatedItem.id) {
-                                   updatedItems.splice(i, 1, updatedItem);
-                                   break;
-                              }
-                         }
+    switch (data.type) {
+        case DtDangKyMoMonGetPage:
+            return Object.assign({}, state, { page: data.page });
+        // case DtDangKyMoMonGetItem:
+        //     return Object.assign({}, state, { item: data.item });
+        case DtDangKyMoMonUpdate:
+            if (state) {
+                let updatedItems = Object.assign({}, state.items),
+                    updatedPage = Object.assign({}, state.page),
+                    updatedItem = data.item;
+                if (updatedItems) {
+                    for (let i = 0, n = updatedItems.length; i < n; i++) {
+                        if (updatedItems[i].id == updatedItem.id) {
+                            updatedItems.splice(i, 1, updatedItem);
+                            break;
+                        }
                     }
-                    if (updatedPage) {
-                         for (let i = 0, n = updatedPage.list.length; i < n; i++) {
-                              if (updatedPage.list[i].id == updatedItem.id) {
-                                   updatedPage.list.splice(i, 1, updatedItem);
-                                   break;
-                              }
-                         }
+                }
+                if (updatedPage) {
+                    for (let i = 0, n = updatedPage.list.length; i < n; i++) {
+                        if (updatedPage.list[i].id == updatedItem.id) {
+                            updatedPage.list.splice(i, 1, updatedItem);
+                            break;
+                        }
                     }
-                    return Object.assign({}, state, { items: updatedItems, page: updatedPage });
-               } else {
-                    return null;
-               }
-          default:
-               return state;
-     }
+                }
+                return Object.assign({}, state, { items: updatedItems, page: updatedPage });
+            } else {
+                return null;
+            }
+        default:
+            return state;
+    }
 }
 
 export const PageName = 'pageDtDangKyMoMon';
 T.initPage(PageName);
+
 export function getDtDangKyMoMonPage(pageNumber, pageSize, pageCondition, done) {
-     const page = T.updatePage(PageName, pageNumber, pageSize, pageCondition);
-     return dispatch => {
-          const url = `/api/dao-tao/dang-ky-mo-mon/page/${page.pageNumber}/${page.pageSize}`;
-          T.get(url, { searchTerm: pageCondition?.searchText, donViFilter: pageCondition?.donViFilter }, data => {
-               if (data.error) {
-                    T.notify('Lấy danh sách đăng ký mở môn bị lỗi!', 'danger');
-                    console.error(`GET ${url}. ${data.error}`);
-               } else {
-                    dispatch({ type: DtDangKyMoMonGetPage, page: data.page });
-                    done && done();
-               }
-          });
-     };
+    const page = T.updatePage(PageName, pageNumber, pageSize, pageCondition);
+    return dispatch => {
+        const url = `/api/dao-tao/dang-ky-mo-mon/page/${page.pageNumber}/${page.pageSize}`;
+        T.get(url, { searchTerm: pageCondition?.searchText, donViFilter: pageCondition?.donViFilter }, data => {
+            if (data.error) {
+                T.notify('Lấy danh sách đăng ký mở môn bị lỗi!', 'danger');
+                console.error(`GET ${url}. ${data.error}`);
+            } else {
+                dispatch({ type: DtDangKyMoMonGetPage, page: data.page });
+                done && done();
+            }
+        });
+    };
 }
 
