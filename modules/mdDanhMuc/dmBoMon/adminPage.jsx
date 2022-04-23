97671db16e8fe3578b173b8d5a8a3f15439f12ee
Hữu Kiệt <kiet.nguyenbk2804@hcmut.edu.vn>
2022-04-23 12:10:02 +0700

    fix dm bo mon

--- modules/mdDanhMuc/dmBoMon/adminPage.jsx
+++ modules/mdDanhMuc/dmBoMon/adminPage.jsx
@@ -10,9 +10,6 @@ class EditModal extends AdminModal {
     DonViTable = [];
 
     componentDidMount() {
-        $(document).ready(() => this.onShown(() => {
-            !this.ma.value() ? this.ma.focus() : this.ten.focus();
-        }));
         this.props.getDataSelect(items => {
             if (items) {
                 this.DonViTable = [];
@@ -25,7 +22,6 @@ class EditModal extends AdminModal {
     onShow = (item) => {
         let { ma, ten, tenTiengAnh, maDv, qdThanhLap, qdXoaTen, kichHoat, ghiChu } = item ? item : { ma: '', ten: '', tenTiengAnh: '', maDv: '', qdThanhLap: '', qdXoaTen: '', kichHoat: false, ghiChu: '' };
         this.setState({ma, item});
-        this.ma.value(ma);
         this.ten.value(ten ? ten : '');
         this.maDv.value(maDv);
         this.tenTiengAnh.value(tenTiengAnh ? tenTiengAnh : '');
@@ -40,7 +36,6 @@ class EditModal extends AdminModal {
     onSubmit = (e) => {
         e.preventDefault();
         const changes = {
-            ma: this.ma.value(),
             ten: this.ten.value(),
             tenTiengAnh: this.tenTiengAnh.value(), 
             maDv: this.maDv.value(),
@@ -49,9 +44,9 @@ class EditModal extends AdminModal {
             kichHoat: this.kichHoat.value() ? 1 : 0,
             ghiChu: this.ghiChu.value(),
         };
-        if (!this.state.ma && !this.ma.value()) {
-            T.notify('Mã bộ môn bị trống!', 'danger');
-            this.ma.focus();
+        if (changes.ten == '') {
+            T.notify('Tên đơn vị thuộc khoa bị trống', 'danger');
+            this.ten.focus();
         } else if (changes.maDv == '') {
             T.notify('Mã đơn vị bị trống!', 'danger');
             this.maDv.focus();
@@ -64,19 +59,17 @@ class EditModal extends AdminModal {
     render = () => {
         const readOnly = this.props.readOnly;
         return this.renderModal({
-            title: this.state.ma ? 'Cập nhật bộ môn' : 'Tạo mới bộ môn',
+            title: this.state.ma ? 'Cập nhật đơn vị thuộc khoa' : 'Tạo mới đơn vị thuộc khoa',
             size: 'large',
             body: <div className='row'>
-                <FormTextBox type='text' className='col-md-6' ref={e => this.ma = e} label='Mã bộ môn' 
-                    readOnly={this.state.ma ? true : readOnly} required />
-                <FormCheckbox className='col-md-6' ref={e => this.kichHoat = e} label='Kích hoạt' isSwitch={true} 
-                    readOnly={readOnly} style={{ display: 'inline-flex', margin: 0 }}
-                    onChange={value => this.changeKichHoat(value ? 1 : 0)} />
-                <FormTextBox type='text' className='col-md-12' ref={e => this.ten = e} label='Tên bộ môn (tiếng Việt)' readOnly={readOnly} />
-                <FormTextBox type='text' className='col-md-12' ref={e => this.tenTiengAnh = e} label='Tên bộ môn (tiếng Anh)' readOnly={readOnly} />
+                <FormTextBox type='text' className='col-md-12' ref={e => this.ten = e} label='Tên đơn vị thuộc khoa (tiếng Việt)' readOnly={readOnly} required/>
+                <FormTextBox type='text' className='col-md-12' ref={e => this.tenTiengAnh = e} label='Tên đơn vị thuộc khoa (tiếng Anh)' readOnly={readOnly} />
                 <FormSelect className='col-md-12' ref={e => this.maDv = e} data={this.DonViTable} label='Mã đơn vị' required />
                 <FormTextBox type='text' className='col-md-12' ref={e => this.qdThanhLap = e} label='Quyết định thành lập' readOnly={readOnly} />
                 <FormTextBox type='text' className='col-12' ref={e => this.qdXoaTen = e} label='Quyết định xóa tên' readOnly={readOnly} />
+                <FormCheckbox className='col-md-6' ref={e => this.kichHoat = e} label='Kích hoạt' isSwitch={true} 
+                    readOnly={readOnly} style={{ display: 'inline-flex', margin: 0 }}
+                    onChange={value => this.changeKichHoat(value ? 1 : 0)} />
                 <FormTextBox type='text' className='col-md-12' ref={e => this.ghiChu = e} label='Ghi chú' readOnly={readOnly} />
             </div>
         });
@@ -107,7 +100,7 @@ class DmBoMonPage extends AdminPage {
     }
 
     delete = (e, item) => {
-        T.confirm('Xóa thông tin', 'Bạn có chắc bạn muốn xóa bộ môn này?', true, isConfirm => isConfirm && this.props.deleteDmBoMon(item.ma));
+        T.confirm('Xóa thông tin', 'Bạn có chắc bạn muốn xóa đơn vị thuộc khoa này?', true, isConfirm => isConfirm && this.props.deleteDmBoMon(item.ma));
         e.preventDefault();
     }
 
@@ -117,13 +110,12 @@ class DmBoMonPage extends AdminPage {
 
         const { pageNumber, pageSize, pageTotal, totalItem, pageCondition, list } = this.props.dmBoMon && this.props.dmBoMon.page ?
         this.props.dmBoMon.page : { pageNumber: 1, pageSize: 50, pageTotal: 1, totalItem: 0, list: null };
-        let table = 'Không có dữ liệu bộ môn';
+        let table = 'Không có dữ liệu đơn vị thuộc khoa';
         if (list && list.length > 0) {
             table = renderTable({
                 getDataSource: () => list, stickyHead: false,
                 renderHead: () => (
                     <tr>
-                        <th style={{ width: 'auto', textAlign: 'center' }}>#</th>
                         <th style={{ width: 'auto', textAlign: 'center' }}>Mã</th>
                         <th style={{ width: '60%' }}>Bộ môn</th>
                         <th style={{ width: '40%' }}>Đơn vị</th>
@@ -133,7 +125,6 @@ class DmBoMonPage extends AdminPage {
                     </tr>),
                 renderRow: (item, index) => (
                     <tr key={index}>
-                        <TableCell type='number' content={(pageNumber - 1) * pageSize + index + 1} />
                         <TableCell type='link' content={item.ma ? item.ma : ''} onClick={() => this.modal.show(item)} />
                         <TableCell type='link' content={<b> {item.ten ? item.ten : ''} <br /> {item.tenTiengAnh ? item.tenTiengAnh : ''} </b>} 
                             className={item.qdXoaTen ? 'text-danger' : 'text-primary'} onClick={() => this.modal.show(item)} />
@@ -148,10 +139,10 @@ class DmBoMonPage extends AdminPage {
         }
         return this.renderPage({
             icon: 'fa fa-list-alt',
-            title: 'Danh mục bộ môn',
+            title: 'Danh mục đơn vị thuộc khoa',
             breadcrumb: [
                 <Link key={0} to='/user/category'>Danh mục</Link>,
-                'Danh mục bộ môn'
+                'Danh mục đơn vị thuộc khoa'
             ],
             content: <>
                 <div className='tile'>{table}</div>
