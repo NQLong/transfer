19d2e85a4c7f18c6cf1dc12bc498bc7b68f65345
TienTran <tientran@TienTrans-Macbook-Air.local>
2022-05-16 18:34:26 +0700

    Gen function

--- modules/mdTccb/qtNghiPhep/model/qtNghiPhep.generate.js
+++ modules/mdTccb/qtNghiPhep/model/qtNghiPhep.generate.js
@@ -145,9 +145,9 @@ module.exports = app => {
                 { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, pagenumber: { val: pagenumber, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, pagesize: { val: pagesize, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, filter, searchterm, totalitem: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER }, pagetotal: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER } }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, done));
         },
 
-        downloadExcel: (filter, done) => {
-            app.database.oracle.connection.main.execute('BEGIN :ret:=qt_nghi_phep_download_excel(:filter); END;',
-                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, filter }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, done));
+        downloadExcel: (filter, searchterm, done) => {
+            app.database.oracle.connection.main.execute('BEGIN :ret:=qt_nghi_phep_download_excel(:filter, :searchterm); END;',
+                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, filter, searchterm }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, done));
         },
     };
 };
