a1f53ccae481f7d007ab42ce0bd6772baaad8a5e
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2022-05-22 03:40:11 +0700

    remove tro lai cong tac, noi dung nghi thai san

--- modules/mdTccb/qtNghiPhep/controller.js
+++ modules/mdTccb/qtNghiPhep/controller.js
@@ -8,7 +8,7 @@ module.exports = app => {
     const menuStaff = {
         parentMenu: app.parentMenu.user,
         menus: {
-            1004: { title: 'Nghỉ phép', subTitle: 'Đăng ký nghỉ phép', link: '/user/nghi-phep', icon: 'fa fa-pause', backgroundColor: '#828593', groupIndex: 1 },
+            1004: { title: 'Nghỉ phép', subTitle: 'Đăng ký nghỉ phép', link: '/user/nghi-phep', icon: 'fa fa-pause', backgroundColor: '#828593', groupIndex: 3 },
         },
     };
 
