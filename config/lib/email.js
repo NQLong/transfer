e84af806370105721a45e5245555c11c792dde1d
TienTran <tientran@TienTrans-Macbook-Air.local>
2022-06-11 10:40:26 +0700

    nodemailer

--- config/lib/email.js
+++ config/lib/email.js
@@ -54,4 +54,37 @@ module.exports = app => {
             return email.endsWith('@hcmussh.edu.vn');
         }
     };
+
+    //Nodemailer
+    const nodemailer = require('nodemailer');
+
+    app.email.sendMail = (mailFrom, mailFromPassword, mailTo, mailCc, mailSubject, mailText, mailHtml, mailAttachments, successCallback, errorCallback) => {
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            auth: { user: mailFrom, pass: mailFromPassword },
+            debug: true
+        });
+        transporter.on('log', console.log);
+
+        const mailOptions = {
+            from: mailFrom,
+            cc: mailCc.toString(),
+            to: mailTo,
+            subject: mailSubject,
+            text: mailText,
+            html: mailHtml,
+            attachments: mailAttachments
+        };
+        transporter.sendMail(mailOptions, error => {
+            if (error) {
+                console.error(error);
+                if (errorCallback) errorCallback(error);
+            } else {
+                console.log('Send mail to ' + mailTo + ' successful.');
+                if (successCallback) successCallback();
+            }
+        });
+    };
+
 };
