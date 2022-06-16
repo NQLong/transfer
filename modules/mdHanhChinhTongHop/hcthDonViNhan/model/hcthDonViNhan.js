f512c9c61ab9776e47f87034f0dce9ad162ec321
LongNguyen <nqlong0709@gmail.com>
2022-06-16 11:21:37 +0700

    refactor async await function

--- modules/mdHanhChinhTongHop/hcthDonViNhan/model/hcthDonViNhan.js
+++ modules/mdHanhChinhTongHop/hcthDonViNhan/model/hcthDonViNhan.js
@@ -1,24 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = app => {
     // app.model.hcthDonViNhan.foo = () => { };
-    const { handleResult } = require('../../constant');
 
-    app.model.hcthDonViNhan.getAllDVN = (condition, selectedColumns, orderBy) => new Promise((resolve, reject) => {
-        app.model.hcthDonViNhan.getAll(condition, selectedColumns, orderBy, (error, item) => {
-            handleResult(resolve, reject, item, error);
-        });
-    });
 
     app.model.hcthDonViNhan.createFromList = (listDonViNhan, ma, loai) => {
-        const promises = listDonViNhan.map(donViNhan => new Promise((resolve, reject) => {
-            app.model.hcthDonViNhan.create({ donViNhan, ma, loai }, (error, item) => {
-                handleResult(resolve, reject, item, error);
-            });
-        }));
+        const promises = listDonViNhan.map(donViNhan => app.model.hcthDonViNhan.create({ donViNhan, ma, loai }));
         return Promise.all(promises);
     };
 
-    app.model.hcthDonViNhan.asyncDelete = (condition) => new Promise((resolve, reject) => {
-        app.model.hcthDonViNhan.delete(condition, (error) => error ? reject(error) : resolve());
-    });
 };
