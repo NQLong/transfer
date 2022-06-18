aa5001c939b1d1c5be3e9e3c554088b637bfc292
HienNP <nphien212@gmail.com>
2022-06-18 17:28:27 +0700

    [temp]update sandbox controller

--- modules/mdKeHoachTaiChinh/tcSetting/adminSettingPage.jsx
+++ modules/mdKeHoachTaiChinh/tcSetting/adminSettingPage.jsx
@@ -35,6 +35,26 @@ class AdminSettingsPage extends AdminPage {
             content: <div className='row'>
                 <div className='col-md-6'>
                     <div className='tile'>
+                        <h3 className='tile-title'>VNPAY (Sandbox)</h3>
+                        <FormTextBox ref={e => this.vnp_TmnCode_Sandbox = e} label='Mã website' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnp_HashSecret_Sandbox = e} label='Secret key' type='password' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnp_Version_Sandbox = e} label='Version VNPay' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnp_Command_Sandbox = e} label='Mã lệnh (default: pay)' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnp_CurrCode_Sandbox = e} label='Đơn vị tiền tệ (defaut: VND)' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnpayUrl_Sandbox = e} label='VNPAY create bill url' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnpayQueryUrl_Sandbox = e} label='VNPAY query bill url' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.vnp_ReturnUrl_Sandbox = e} label='URL sau khi thanh toán thành công' readOnly={readOnly} />
+
+                        <div style={{ textAlign: 'right' }}>
+                            <button className='btn btn-success' type='button' onClick={() => this.save('vnp_TmnCode_Sandbox', 'vnp_HashSecret_Sandbox', 'vnp_Version_Sandbox', 'vnp_Command_Sandbox', 'vnp_CurrCode_Sandbox', 'vnp_ReturnUrl_Sandbox', 'vnpayUrl_Sandbox', 'vnpayQueryUrl_Sandbox')}>
+                                <i className='fa fa-fw fa-lg fa-save'></i>Lưu
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='col-md-6'>
+                    <div className='tile'>
                         <h3 className='tile-title'>VNPAY</h3>
                         <FormTextBox ref={e => this.vnp_TmnCode = e} label='Mã website' readOnly={readOnly} />
                         <FormTextBox ref={e => this.vnp_HashSecret = e} label='Secret key' type='password' readOnly={readOnly} />
@@ -43,9 +63,7 @@ class AdminSettingsPage extends AdminPage {
                         <FormTextBox ref={e => this.vnp_CurrCode = e} label='Đơn vị tiền tệ (defaut: VND)' readOnly={readOnly} />
                         <FormTextBox ref={e => this.vnpayUrl = e} label='VNPAY create bill url' readOnly={readOnly} />
                         <FormTextBox ref={e => this.vnpayQueryUrl = e} label='VNPAY query bill url' readOnly={readOnly} />
-
                         <FormTextBox ref={e => this.vnp_ReturnUrl = e} label='URL sau khi thanh toán thành công' readOnly={readOnly} />
-
                         <div style={{ textAlign: 'right' }}>
                             <button className='btn btn-success' type='button' onClick={() => this.save('vnp_TmnCode', 'vnp_HashSecret', 'vnp_Version', 'vnp_Command', 'vnp_CurrCode', 'vnp_ReturnUrl', 'vnpayUrl', 'vnpayQueryUrl')}>
                                 <i className='fa fa-fw fa-lg fa-save'></i>Lưu
@@ -53,6 +71,19 @@ class AdminSettingsPage extends AdminPage {
                         </div>
                     </div>
 
+                </div>
+                <div className='col-md-6'>
+                    <div className='tile'>
+                        <h3 className='tile-title'>Ngân hàng (Sandbox): Chuỗi bí mật</h3>
+                        <FormTextBox ref={e => this.secretCodeBidvSandbox = e} label='BIDV' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.secretCodeVcbSandbox = e} label='VCB' readOnly={readOnly} />
+                        <FormTextBox ref={e => this.secretCodeAgriSandbox = e} label='Agribank' readOnly={readOnly} />
+                        <div style={{ textAlign: 'right' }}>
+                            <button className='btn btn-success' type='button' onClick={() => this.save('secretCodeBidvSandbox', 'secretCodeVcbSandbox', 'secretCodeAgriSandbox')}>
+                                <i className='fa fa-fw fa-lg fa-save'></i>Lưu
+                            </button>
+                        </div>
+                    </div> 
                 </div>
                 <div className='col-md-6'>
                     <div className='tile'>
@@ -65,9 +96,7 @@ class AdminSettingsPage extends AdminPage {
                                 <i className='fa fa-fw fa-lg fa-save'></i>Lưu
                             </button>
                         </div>
-                    </div>
-
-
+                    </div> 
                 </div>
             </div>,
         });
