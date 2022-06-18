dec5fb4e68ee335cb8f329bec72f5eef8ae6112a
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2022-06-18 17:21:49 +0700

    Init

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,7 +1,4 @@
 export default {
-    // API_HOST: 'tuyensinhdh.hcmussh.edu.vn',
-    API_URL: 'https://tuyensinhdh.hcmussh.edu.vn/',
-    // API_URL: 'http://172.28.3.183:8440',
-    // API_URL: 'http://192.168.2.5:8440',
-    // API_URL: 'http://10.127.30.111:8440',
+    API_URL: 'https://hcmussh.edu.vn/',
+    // API_URL: 'http://172.28.3.183:7012',
 };
