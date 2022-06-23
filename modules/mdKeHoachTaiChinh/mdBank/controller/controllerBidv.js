bca1d1db509b59aeaa10e357167d6821ad7d6a7f
TienTran <tientran@TienTrans-Macbook-Air.local>
2022-06-23 16:33:17 +0700

    parseInt Amount

--- modules/mdKeHoachTaiChinh/mdBank/controller/controllerBidv.js
+++ modules/mdKeHoachTaiChinh/mdBank/controller/controllerBidv.js
@@ -113,7 +113,7 @@ module.exports = app => {
                 } else if (!hocPhi) {
                     res.send({ result_code: '025' });
                 } else {
-                    modelHocPhiTransaction.addBill(namHoc, hocKy, 'BIDV', `BIDV-${trans_id}`, app.date.fullFormatToDate(trans_date).getTime(), customer_id, bill_id, service_id, amount, checksum, (error, result) => {
+                    modelHocPhiTransaction.addBill(namHoc, hocKy, 'BIDV', `BIDV-${trans_id}`, app.date.fullFormatToDate(trans_date).getTime(), customer_id, bill_id, service_id, parseInt(amount), checksum, (error, result) => {
                         if (error || !result || !result.outBinds || !result.outBinds.ret) {
                             res.send({ result_code: '096' });
                         } else {
