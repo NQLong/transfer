077f40f06e9e3206cf49aff3e81fff910b8160fc
TienTran <tientran@TienTrans-Macbook-Air.local>
2022-06-23 16:21:20 +0700

    gen sandbox

--- modules/mdKeHoachTaiChinh/tcSandBox/model/tcHocPhiSandbox.generate.js
+++ modules/mdKeHoachTaiChinh/tcSandBox/model/tcHocPhiSandbox.generate.js
@@ -215,18 +215,5 @@ module.exports = app => {
                 }
             });
         }),
-
-        searchPage: (pagenumber, pagesize, imssv, searchterm, filter, done) => new Promise((resolve, reject) => {
-            app.database.oracle.connection.main.execute('BEGIN :ret:=tc_hoc_phi_search_page(:pagenumber, :pagesize, :imssv, :searchterm, :filter, :totalitem, :pagetotal); END;',
-                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, pagenumber: { val: pagenumber, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, pagesize: { val: pagesize, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, imssv, searchterm, filter, totalitem: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER }, pagetotal: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER } }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, (error, result) => {
-                    if (error) {
-                        done && done(error);
-                        reject(error);
-                    } else {
-                        done && done(null, result);
-                        resolve(result);
-                    }
-                }));
-        }),
     };
 };
