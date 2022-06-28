86021edc8fecc29a5868def9368ed8d6e5f89980
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2022-06-28 15:38:50 +0700

    temp

--- view/component/MultipleLanguageForm.jsx
+++ view/component/MultipleLanguageForm.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export class FormMultipleLanguage extends React.Component {
+    state = {  };
 
+    componentDidMount() {
+        $(document).ready(() => {
+            // const { tabRender = false, languages = ['vi', 'en'], title, gridClassName = 'col-md-12', className = '', formElement, readOnly, required } = this.props;
+            // languages.forEach(code => {
+            //
+            // });
+
+        });
+    }
+
+    render() {
+        // const {} = this.props;
+        return <div>
+
+        </div>;
+    }
 }
