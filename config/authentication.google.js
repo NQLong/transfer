22174153699a950a43537da74bf0eac023b187a4
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2022-07-01 17:39:56 +0700

    deploy

--- config/authentication.google.js
+++ config/authentication.google.js
@@ -77,7 +77,7 @@ module.exports = (app) => {
     app.verifyIdToken = async (token) => {
         const ticket = await app.googleO2client.verifyIdToken({
             idToken: token,
-            audience: GoogleStrategyConfig.clientID,  // Specify the CLIENT_ID of the app that accesses the backend
+            audience: GoogleStrategyConfig.clientID  // Specify the CLIENT_ID of the app that accesses the backend
         });
         const payload = ticket.getPayload();
         return payload;
@@ -97,14 +97,13 @@ module.exports = (app) => {
                 throw 'Tài khoản không hợp lệ';
             else {
                 const user = await app.model.fwUser.get({ email: userEmail });
-                console.log({userEmail, user});
+                // console.log({ userEmail, user });
                 if (!user) throw 'Tài khoản không tồn tại';
                 app.updateSessionUser(req, user, () => {
                     res.send({ message: 'Đăng nhập thành công', email: user.email });
                 });
             }
-        }
-        catch (error) {
+        } catch (error) {
             res.status(401).send({ message: 'Đăng nhập không thành công', error });
         }
     });
