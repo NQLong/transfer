727573132855c32212d8086fe1426577cabe9e9d
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2022-07-11 11:28:56 +0700

    1.0.154

--- asset/db/methods.changes.main.sql
+++ asset/db/methods.changes.main.sql
@@ -50,6 +50,88 @@ END;
 /
 --EndMethod--
 
+CREATE OR REPLACE FUNCTION DT_KHUNG_DAO_TAO_SEARCH_PAGE(pageNumber IN OUT NUMBER, pageSize IN OUT NUMBER,
+                                             searchTerm IN STRING, filter IN STRING,
+                                             totalItem OUT NUMBER, pageTotal OUT NUMBER) RETURN SYS_REFCURSOR
+AS
+    my_cursor          SYS_REFCURSOR;
+    sT                 STRING(502) := '%' || lower(searchTerm) || '%';
+    donVi              STRING(10);
+    namDaoTao          NUMBER(4);
+    listLoaiHinhDaoTao STRING(50);
+BEGIN
+    SELECT JSON_VALUE(filter, '$.donVi') INTO donVi FROM DUAL;
+    SELECT JSON_VALUE(filter, '$.namDaoTao') INTO namDaoTao FROM DUAL;
+    SELECT JSON_VALUE(filter, '$.listLoaiHinhDaoTao') INTO listLoaiHinhDaoTao FROM DUAL;
+
+    SELECT COUNT(*)
+    INTO totalItem
+    FROM DT_KHUNG_DAO_TAO KDT
+             LEFT JOIN DT_DANH_SACH_CHUYEN_NGANH CN ON CN.ID = KDT.CHUYEN_NGANH
+             LEFT JOIN DM_DON_VI DV ON DV.MA = KDT.MA_KHOA
+             LEFT JOIN DT_NGANH_DAO_TAO DNDT on KDT.MA_NGANH = DNDT.MA_NGANH
+             LEFT JOIN DT_CAU_TRUC_KHUNG_DAO_TAO KHUNG ON KHUNG.ID = KDT.NAM_DAO_TAO
+
+    WHERE (donVi IS NULL OR donVi = '' OR TO_NUMBER(donVi) = KDT.MA_KHOA)
+      AND (listLoaiHinhDaoTao IS NULL OR
+           listLoaiHinhDaoTao IS NOT NULL AND
+           KDT.LOAI_HINH_DAO_TAO IN (SELECT regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level)
+                                     from dual
+                                     connect by regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level) is not null))
+      AND (searchTerm = ''
+        OR LOWER(TRIM(DNDT.MA_NGANH)) LIKE sT
+        OR LOWER(TRIM(DNDT.TEN_NGANH)) LIKE sT
+        OR LOWER(TRIM(KDT.NAM_DAO_TAO)) LIKE sT
+        OR LOWER(TRIM(DV.TEN)) LIKE sT)
+      AND (namDaoTao IS NULL OR namDaoTao = '' OR TO_NUMBER(namDaoTao) = KDT.NAM_DAO_TAO);
+
+    IF pageNumber < 1 THEN pageNumber := 1; END IF;
+    IF pageSize < 1 THEN pageSize := 1; END IF;
+    pageTotal := CEIL(totalItem / pageSize);
+    pageNumber := LEAST(pageNumber, pageTotal);
+
+    OPEN my_cursor FOR
+        SELECT *
+        FROM (SELECT KDT.MA_KHOA           AS                          "maKhoa",
+                     KDT.NAM_DAO_TAO       AS                          "idNamDaoTao",
+                     KHUNG.NAM_DAO_TAO     AS                          "namDaoTao",
+                     KDT.ID                AS                          "id",
+                     KDT.MA_NGANH          AS                          "maNganh",
+                     DNDT.TEN_NGANH        AS                          "tenNganh",
+                     BDT.TEN_BAC           AS                          "trinhDoDaoTao",
+                     LHDT.TEN              AS                          "loaiHinhDaoTao",
+                     KDT.THOI_GIAN_DAO_TAO AS                          "thoiGianDaoTao",
+                     DV.TEN                AS                          "tenKhoaBoMon",
+                     CN.TEN                AS                          "tenChuyenNganh",
+                     ROW_NUMBER() OVER (ORDER BY KDT.NAM_DAO_TAO DESC) R
+              FROM DT_KHUNG_DAO_TAO KDT
+                       LEFT JOIN DT_DANH_SACH_CHUYEN_NGANH CN ON CN.ID = KDT.CHUYEN_NGANH
+                       LEFT JOIN DM_DON_VI DV ON DV.MA = KDT.MA_KHOA
+                       LEFT JOIN DT_NGANH_DAO_TAO DNDT on KDT.MA_NGANH = DNDT.MA_NGANH
+                       LEFT JOIN DT_CAU_TRUC_KHUNG_DAO_TAO KHUNG ON KHUNG.ID = KDT.NAM_DAO_TAO
+
+                       LEFT JOIN DM_SV_BAC_DAO_TAO BDT ON BDT.MA_BAC = KDT.TRINH_DO_DAO_TAO
+                       LEFT JOIN DM_SV_LOAI_HINH_DAO_TAO LHDT ON LHDT.MA = KDT.LOAI_HINH_DAO_TAO
+              WHERE (donVi IS NULL OR donVi = '' OR TO_NUMBER(donVi) = KDT.MA_KHOA)
+                AND (listLoaiHinhDaoTao IS NULL OR
+                     listLoaiHinhDaoTao IS NOT NULL AND
+                     KDT.LOAI_HINH_DAO_TAO IN (SELECT regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level)
+                                               from dual
+                                               connect by regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level) is not null))
+                AND (searchTerm = ''
+                  OR LOWER(TRIM(DNDT.MA_NGANH)) LIKE sT
+                  OR LOWER(TRIM(DNDT.TEN_NGANH)) LIKE sT
+                  OR LOWER(TRIM(KDT.NAM_DAO_TAO)) LIKE sT
+                  OR LOWER(TRIM(DV.TEN)) LIKE sT)
+                AND (namDaoTao IS NULL OR namDaoTao = '' OR TO_NUMBER(namDaoTao) = KDT.NAM_DAO_TAO)
+              ORDER BY KDT.NAM_DAO_TAO DESC)
+        WHERE R BETWEEN (pageNumber - 1) * pageSize + 1 AND pageNumber * pageSize;
+    RETURN my_cursor;
+END;
+
+/
+--EndMethod--
+
 CREATE OR REPLACE FUNCTION TC_HOC_PHI_SEARCH_PAGE(pageNumber IN OUT NUMBER, pageSize IN OUT NUMBER, imssv IN STRING,
                                        searchTerm IN STRING, filter IN STRING,
                                        totalItem OUT NUMBER, pageTotal OUT NUMBER, totalCurrent OUT NUMBER,
@@ -128,9 +210,13 @@ BEGIN
                      HP.CONG_NO                          AS "congNo",
                      HP.HOC_PHI                          AS "hocPhi",
                      (FS.HO || ' ' || FS.TEN)            AS "hoTenSinhVien",
+                     FS.DIEN_THOAI_CA_NHAN               AS "soDienThoai",
+                     FS.EMAIL_CA_NHAN                    AS "emailCaNhan",
                      FS.MA_NGANH                         AS "maNganh",
                      NDT.TEN_NGANH                       AS "tenNganh",
                      DV.TEN                              AS "tenKhoa",
+                     LHDT.TEN                            AS "tenLoaiHinhDaoTao",
+                     BDT.TEN_BAC                         AS "tenBac",
                      (SELECT MAX(TRANS_DATE)
                       FROM TC_HOC_PHI_TRANSACTION TRANS
                       WHERE HP.HOC_KY = TRANS.HOC_KY
@@ -141,6 +227,8 @@ BEGIN
                        LEFT JOIN FW_STUDENT FS on HP.MSSV = FS.MSSV
                        LEFT JOIN DT_NGANH_DAO_TAO NDT on FS.MA_NGANH = NDT.MA_NGANH
                        LEFT JOIN DM_DON_VI DV ON DV.MA = NDT.KHOA
+                       LEFT JOIN DM_SV_LOAI_HINH_DAO_TAO LHDT ON FS.LOAI_HINH_DAO_TAO = LHDT.MA
+                       LEFT JOIN DM_SV_BAC_DAO_TAO BDT on BDT.MA_BAC = FS.BAC_DAO_TAO
               WHERE (imssv IS NULL OR imssv = '' OR imssv = HP.MSSV)
                 AND (
                       daDong IS NULL OR daDong = ''
--- asset/db/methods.main.sql
+++ asset/db/methods.main.sql
@@ -969,13 +969,20 @@ END;
 /
 --EndMethod--
 
-CREATE OR REPLACE FUNCTION DT_KHUNG_DAO_TAO_SEARCH_PAGE(pageNumber IN OUT NUMBER, pageSize IN OUT NUMBER, donVi IN STRING,
-                                             searchTerm IN STRING, namDaoTao IN STRING,
+CREATE OR REPLACE FUNCTION DT_KHUNG_DAO_TAO_SEARCH_PAGE(pageNumber IN OUT NUMBER, pageSize IN OUT NUMBER,
+                                             searchTerm IN STRING, filter IN STRING,
                                              totalItem OUT NUMBER, pageTotal OUT NUMBER) RETURN SYS_REFCURSOR
 AS
-    my_cursor SYS_REFCURSOR;
-    sT        STRING(502) := '%' || lower(searchTerm) || '%';
+    my_cursor          SYS_REFCURSOR;
+    sT                 STRING(502) := '%' || lower(searchTerm) || '%';
+    donVi              STRING(10);
+    namDaoTao          NUMBER(4);
+    listLoaiHinhDaoTao STRING(50);
 BEGIN
+    SELECT JSON_VALUE(filter, '$.donVi') INTO donVi FROM DUAL;
+    SELECT JSON_VALUE(filter, '$.namDaoTao') INTO namDaoTao FROM DUAL;
+    SELECT JSON_VALUE(filter, '$.listLoaiHinhDaoTao') INTO listLoaiHinhDaoTao FROM DUAL;
+
     SELECT COUNT(*)
     INTO totalItem
     FROM DT_KHUNG_DAO_TAO KDT
@@ -985,6 +992,11 @@ BEGIN
              LEFT JOIN DT_CAU_TRUC_KHUNG_DAO_TAO KHUNG ON KHUNG.ID = KDT.NAM_DAO_TAO
 
     WHERE (donVi IS NULL OR donVi = '' OR TO_NUMBER(donVi) = KDT.MA_KHOA)
+      AND (listLoaiHinhDaoTao IS NULL OR
+           listLoaiHinhDaoTao IS NOT NULL AND
+           KDT.LOAI_HINH_DAO_TAO IN (SELECT regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level)
+                                     from dual
+                                     connect by regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level) is not null))
       AND (searchTerm = ''
         OR LOWER(TRIM(DNDT.MA_NGANH)) LIKE sT
         OR LOWER(TRIM(DNDT.TEN_NGANH)) LIKE sT
@@ -1020,6 +1032,11 @@ BEGIN
                        LEFT JOIN DM_SV_BAC_DAO_TAO BDT ON BDT.MA_BAC = KDT.TRINH_DO_DAO_TAO
                        LEFT JOIN DM_SV_LOAI_HINH_DAO_TAO LHDT ON LHDT.MA = KDT.LOAI_HINH_DAO_TAO
               WHERE (donVi IS NULL OR donVi = '' OR TO_NUMBER(donVi) = KDT.MA_KHOA)
+                AND (listLoaiHinhDaoTao IS NULL OR
+                     listLoaiHinhDaoTao IS NOT NULL AND
+                     KDT.LOAI_HINH_DAO_TAO IN (SELECT regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level)
+                                               from dual
+                                               connect by regexp_substr(listLoaiHinhDaoTao, '[^,]+', 1, level) is not null))
                 AND (searchTerm = ''
                   OR LOWER(TRIM(DNDT.MA_NGANH)) LIKE sT
                   OR LOWER(TRIM(DNDT.TEN_NGANH)) LIKE sT
@@ -15471,9 +15488,13 @@ BEGIN
                      HP.CONG_NO                          AS "congNo",
                      HP.HOC_PHI                          AS "hocPhi",
                      (FS.HO || ' ' || FS.TEN)            AS "hoTenSinhVien",
+                     FS.DIEN_THOAI_CA_NHAN               AS "soDienThoai",
+                     FS.EMAIL_CA_NHAN                    AS "emailCaNhan",
                      FS.MA_NGANH                         AS "maNganh",
                      NDT.TEN_NGANH                       AS "tenNganh",
                      DV.TEN                              AS "tenKhoa",
+                     LHDT.TEN                            AS "tenLoaiHinhDaoTao",
+                     BDT.TEN_BAC                         AS "tenBac",
                      (SELECT MAX(TRANS_DATE)
                       FROM TC_HOC_PHI_TRANSACTION TRANS
                       WHERE HP.HOC_KY = TRANS.HOC_KY
@@ -15484,6 +15505,8 @@ BEGIN
                        LEFT JOIN FW_STUDENT FS on HP.MSSV = FS.MSSV
                        LEFT JOIN DT_NGANH_DAO_TAO NDT on FS.MA_NGANH = NDT.MA_NGANH
                        LEFT JOIN DM_DON_VI DV ON DV.MA = NDT.KHOA
+                       LEFT JOIN DM_SV_LOAI_HINH_DAO_TAO LHDT ON FS.LOAI_HINH_DAO_TAO = LHDT.MA
+                       LEFT JOIN DM_SV_BAC_DAO_TAO BDT on BDT.MA_BAC = FS.BAC_DAO_TAO
               WHERE (imssv IS NULL OR imssv = '' OR imssv = HP.MSSV)
                 AND (
                       daDong IS NULL OR daDong = ''
