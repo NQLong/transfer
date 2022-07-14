9d1eb4b6c01b839848f8c1bb7d6a735c493c5dd2
Long Nguyen <nqlong0709@gmail.com>
2022-07-15 05:00:44 +0700

    Feat: implement transaction list page

---
+++ modules/mdKeHoachTaiChinh/tcDanhSachGiaoDich/index.jsx
@@ -0,0 +1,18 @@
+//TEMPLATES: admin
+
+import Loadable from 'react-loadable';
+import Loading from 'view/component/Loading';
+import tcGiaoDich from './redux';
+
+export default {
+    redux: {
+        parent: 'finance',
+        reducers: { tcGiaoDich }
+    },
+    routes: [
+        {
+            path: '/user/finance/danh-sach-giao-dich',
+            component: Loadable({ loading: Loading, loader: () => import('./adminPage') })
+        },
+    ],
+};
--- modules/mdKeHoachTaiChinh/tcHocPhi/model/tcHocPhiTransaction.generate.js
+++ modules/mdKeHoachTaiChinh/tcHocPhi/model/tcHocPhiTransaction.generate.js
@@ -228,5 +228,31 @@ module.exports = app => {
                     }
                 });
         }),
+
+        searchPage: (pagenumber, pagesize, searchterm, filter, done) => new Promise((resolve, reject) => {
+            app.database.oracle.connection.main.execute('BEGIN :ret:=tc_hoc_phi_transaction_search_page(:pagenumber, :pagesize, :searchterm, :filter, :totalitem, :pagetotal); END;',
+                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, pagenumber: { val: pagenumber, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, pagesize: { val: pagesize, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, searchterm, filter, totalitem: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER }, pagetotal: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER } }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, (error, result) => {
+                    if (error) {
+                        done && done(error);
+                        reject(error);
+                    } else {
+                        done && done(null, result);
+                        resolve(result);
+                    }
+                }));
+        }),
+
+        downloadPsc: (filter, done) => new Promise((resolve, reject) => {
+            app.database.oracle.connection.main.execute('BEGIN :ret:=tc_hoc_phi_transaction_download_psc(:filter); END;',
+                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, filter }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, (error, result) => {
+                    if (error) {
+                        done && done(error);
+                        reject(error);
+                    } else {
+                        done && done(null, result);
+                        resolve(result);
+                    }
+                }));
+        }),
     };
 };
