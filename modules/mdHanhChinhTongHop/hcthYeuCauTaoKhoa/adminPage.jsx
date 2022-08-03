44d4b2a098f0ae80f4d960c4e74f905a9c542245
LongNguyen <nqlong0709@gmail.com>
2022-08-03 11:30:07 +0700

    Feat: keystore request

--- modules/mdHanhChinhTongHop/hcthYeuCauTaoKhoa/adminPage.jsx
+++ modules/mdHanhChinhTongHop/hcthYeuCauTaoKhoa/adminPage.jsx
@@ -1,24 +1,60 @@
 import React from 'react';
-import { AdminPage, renderTable } from 'view/component/AdminPage';
+import { AdminPage, renderTable, TableCell } from 'view/component/AdminPage';
 import { connect } from 'react-redux';
-import { TableCell } from '@mui/material';
-
+import { getRequest, updateStatus } from './redux';
+const { trangThaiRequest } = require('../constant');
 export class YeuCauTaoKhoa extends AdminPage {
+
+    state = { filter: {} }
     componentDidMount() {
         T.ready(this.pageConfig.ready, () => {
-
-        })
+            this.props.getRequest();
+            this.changeAdvancedSearch(true);
+        });
     }
 
-    renderTable = () => renderTable({
-        getDataSource: () => [{}, {}, {}, {}, {}, {}, {}, {},],
+    changeAdvancedSearch = (isInitial) => {
+        let { pageNumber, pageSize } = this.props && this.props.hcthYeuCauTaoKhoa && this.props.hcthYeuCauTaoKhoa.page ? this.props.hcthYeuCauTaoKhoa.page : { pageNumber: 1, pageSize: 50 };
+        const pageFilter = isInitial ? {} : {};
+
+        this.setState({ filter: pageFilter }, () => {
+            this.getPage(pageNumber, pageSize, '', (page) => {
+                if (isInitial) {
+                    const filter = page.filter || {};
+                    this.setState({ filter: !$.isEmptyObject(filter) ? filter : pageFilter });
+                    // if (!$.isEmptyObject(filter) && filter && (filter.donViNhan || filter.canBoNhan)) this.showAdvanceSearch();
+                }
+            });
+        });
+    }
+
+    getPage = (pageNumber, pageSize, pageCondition, done) => {
+        this.props.getRequest(pageNumber, pageSize, pageCondition, this.state.filter, done);
+    }
+
+    onAprroveRequest = (request) => {
+        T.confirm('Duyệt yêu cầu này', `Bạn có chắc muốn duyệt yêu cầu tạo khóa của ${`${request.ho} ${request.ten}`.trim().normalizedName()}?`, true,
+            isConfirm => isConfirm && this.props.updateStatus(request.id, trangThaiRequest.DA_DUYET.id));
+    }
+
+    onRefuseRequest = (request) => {
+        T.confirm('Từ chối yêu cầu này', `Bạn có chắc muốn từ chối yêu cầu tạo khóa của ${`${request.ho} ${request.ten}`.trim().normalizedName()}?`, true,
+            isConfirm => isConfirm && this.props.updateStatus(request.id, trangThaiRequest.TU_CHOI.id));
+    }
+
+    renderTable = (permissions) => renderTable({
+        getDataSource: () => this.props.hcthYeuCauTaoKhoa?.page?.list,
         // header: 'theadlight',
+        loadingOverlay: false,
+        loadingClassName: 'd-flex justify-content-center',
+        stickyHead: true,
         emptyTable: 'Chưa có yêu cầu tạo khóa',
         renderHead: () => <tr>
             <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>#</th>
             <th style={{ width: '100%', whiteSpace: 'nowrap' }}>Người yêu cầu</th>
             <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Ngày yêu cầu</th>
-            <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Ngày duyệt</th>
+            <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Ngày cập nhật</th>
+            <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Cập nhật bởi</th>
             <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Trạng thái</th>
             <th style={{ width: 'auto', whiteSpace: 'nowrap' }}>Thao tác</th>
         </tr>,
@@ -27,10 +63,21 @@ export class YeuCauTaoKhoa extends AdminPage {
                 <TableCell style={{ whiteSpace: 'nowrap' }} content={item.R || index} />
                 <TableCell style={{ whiteSpace: 'nowrap' }} content={`${item.shcc}: ` + `${item.ho} ${item.ten}`.trim().normalizedName()} />
                 <TableCell style={{ whiteSpace: 'nowrap' }} content={item.ngayTao && T.dateToText(new Date(item.ngayTao), 'HH:MM dd/mm/yyyy')} />
-                <TableCell style={{ whiteSpace: 'nowrap' }} content={item.ngayDuyet && T.dateToText(new Date(item.ngayDuyet), 'HH:MM dd/mm/yyyy')} />
-                <TableCell style={{ whiteSpace: 'nowrap' }} content={item.trangThai} />
-                <TableCell style={{ whiteSpace: 'nowrap' }} content={''} />
-            </tr>
+                <TableCell style={{ whiteSpace: 'nowrap' }} content={item.ngayCapNhat && T.dateToText(new Date(item.ngayCapNhat), 'HH:MM dd/mm/yyyy')} />
+                <TableCell style={{ whiteSpace: 'nowrap' }} content={item.shccCanBoCapNhat ? `${item.shccCanBoCapNhat}: ` + `${item.hoCanBoCapNhat} ${item.tenCanBoCapNhat}`.trim().normalizedName() : ''} />
+                <TableCell style={{ whiteSpace: 'nowrap' }} content={item.trangThai ? trangThaiRequest[item.trangThai]?.text : ''} />
+                <TableCell type='buttons' style={{ whiteSpace: 'nowrap', textAlign: 'left' }} >
+                    {permissions?.write && item.trangThai === trangThaiRequest.CHO_DUYET.id && <>
+                        <button className='btn btn-success' onClick={() => this.onAprroveRequest(item)}>
+                            <i className='fa fa-lg fa-check' />
+                        </button>
+                        <button className='btn btn-danger' onClick={() => this.onRefuseRequest(item)}>
+                            <i className='fa fa-lg fa-times' />
+                        </button>
+                    </>
+                    }
+                </TableCell>
+            </tr>;
         }
     });
 
@@ -51,7 +98,7 @@ export class YeuCauTaoKhoa extends AdminPage {
             icon: this.pageConfig.icon,
             content: <div className='tile row'>
                 <div className='col-md-12'>
-                    {this.renderTable()}
+                    {this.renderTable(permissions)}
                 </div>
             </div>
         });
@@ -59,5 +106,5 @@ export class YeuCauTaoKhoa extends AdminPage {
 }
 
 const mapStateToProps = state => ({ system: state.system, hcthYeuCauTaoKhoa: state.hcth.hcthYeuCauTaoKhoa });
-const mapActionsToProps = {};
+const mapActionsToProps = { getRequest, updateStatus };
 export default connect(mapStateToProps, mapActionsToProps)(YeuCauTaoKhoa);
--- modules/mdHanhChinhTongHop/hcthYeuCauTaoKhoa/controller.js
+++ modules/mdHanhChinhTongHop/hcthYeuCauTaoKhoa/controller.js
@@ -1,4 +1,7 @@
 module.exports = app => {
+    const forge = require('node-forge');
+    const { trangThaiRequest } = require('../constant');
+
     const staffMenu = {
         parentMenu: app.parentMenu.hcth,
         menus: {
@@ -13,27 +16,244 @@ module.exports = app => {
         },
     };
 
-    app.permission.add({ name: 'hcthYeuCauTaoKhoa:read', menu: staffMenu });
+    app.permission.add({ name: 'hcthYeuCauTaoKhoa:read', menu: staffMenu }, 'hcthYeuCauTaoKhoa:write', 'hcthYeuCauTaoKhoa:delete');
     app.permission.add({ name: 'manager:write', menu: menu });
     app.get('/user/hcth/yeu-cau-tao-khoa', app.permission.check('hcthYeuCauTaoKhoa:read'), app.templates.admin);
     app.get('/user/yeu-cau-tao-khoa', app.permission.check('hcthYeuCauTaoKhoa:read'), app.templates.admin);
 
-    app.get('/api/hcth/yeu-cau-tao-khoa/page/:pageNumber/:pageSize', app.permission.check('hcthYeuCauTaoKhoa:read'), async (req, res) => {
+    app.get('/api/hcth/yeu-cau-tao-khoa/user/page/:pageNumber/:pageSize', app.permission.check('staff:login'), async (req, res) => {
         try {
-            res.send({ items: [] });
+            const filter = req.query.filter || {};
+            const shcc = req.session.user.shcc;
+            filter.canBoTao = shcc;
+            const filterData = app.stringify(filter);
+            const pageCondition = req.query.searchTerm;
+            const page = await app.model.hcthYeuCauTaoKhoa.searchPage(parseInt(req.params.pageNumber), parseInt(req.params.pageSize), pageCondition, filterData);
+            const { totalitem: totalItem, pagesize: pageSize, pagetotal: pageTotal, pagenumber: pageNumber, rows: list } = page;
+            res.send({
+                page: { totalItem, pageSize, pageTotal, pageNumber, pageCondition, list, filter }
+            });
+            // res.send({ items: [] });
         } catch (error) {
-            res.send({error});
+            res.send({ error });
         }
     });
 
+    app.get('/api/hcth/khoa/user', app.permission.orCheck('rectors:login', 'manager:write'), async (req, res) => {
+        try {
+            const shcc = req.session.user.shcc;
+            const khoa = await app.model.hcthUserPublicKey.get({ shcc, kichHoat: 1 });
+            if (khoa) {
+                const yeuCau = await app.model.hcthYeuCauTaoKhoa.get({ id: khoa.yeuCau });
+                res.send({ item: { khoa, yeuCau } });
+            } else {
+                res.send({ item: {} });
+            }
+        } catch (error) {
+            res.send({ error });
+        }
+    });
+
+
+    const genKey = async (id, shcc, passphrase) => {
+        // const fs = require('fs');
+        const setting = await app.model.hcthSetting.getValue('rootPassphrase');
+        const privateKey = app.fs.readFileSync(app.path.join(app.assetPath, 'ca/hcmussh.key'), 'utf-8');
+        const cert = app.fs.readFileSync(app.path.join(app.assetPath, 'ca/hcmussh.pem'), 'utf-8');
+        // const caKey = (forge.pki.decryptRsaPrivateKey(privateKey, 'benn1904'));
+        const caKey = forge.pki.decryptRsaPrivateKey(privateKey, setting.rootPassphrase);
+        const caCert = forge.pki.certificateFromPem(cert);
+        const hostKeys = forge.pki.rsa.generateKeyPair(2048);
+        const getCertNotBefore = () => {
+            let twoDaysAgo = new Date(Date.now() - 60 * 60 * 24 * 2 * 1000);
+            let year = twoDaysAgo.getFullYear();
+            let month = (twoDaysAgo.getMonth() + 1).toString().padStart(2, '0');
+            let day = twoDaysAgo.getDate();
+            return new Date(`${year}-${month}-${day} 00:00:00Z`);
+        };
+
+        // Get Certificate Expiration Date (Valid for 365 Days)
+        const getCertNotAfter = (notBefore) => {
+            let ninetyDaysLater = new Date(notBefore.getTime() + 60 * 60 * 24 * 365 * 1000);
+            let year = ninetyDaysLater.getFullYear();
+            let month = (ninetyDaysLater.getMonth() + 1).toString().padStart(2, '0');
+            let day = ninetyDaysLater.getDate();
+            return new Date(`${year}-${month}-${day} 23:59:59Z`);
+        };
+
+        // // Get CA Expiration Date (Valid for 100 Years)
+        // const getCANotAfter = (notBefore) => {
+        //     let year = notBefore.getFullYear() + 100;
+        //     let month = (notBefore.getMonth() + 1).toString().padStart(2, '0');
+        //     let day = notBefore.getDate();
+        //     return new Date(`${year}-${month}-${day} 23:59:59Z`);
+        // }
+
+        const makeNumberPositive = (hexString) => {
+            let mostSignificativeHexDigitAsInt = parseInt(hexString[0], 16);
+
+            if (mostSignificativeHexDigitAsInt < 8) return hexString;
+
+            mostSignificativeHexDigitAsInt -= 8;
+            return mostSignificativeHexDigitAsInt.toString() + hexString.substring(1);
+        };
+
+        // Generate a random serial number for the Certificate
+        const randomSerialNumber = () => {
+            return makeNumberPositive(forge.util.bytesToHex(forge.random.getBytesSync(20)));
+        };
+        // Define the attributes/properties for the Host Certificate
+        const attributes = [{
+            shortName: 'C',
+            value: 'VN'
+        }, {
+            shortName: 'ST',
+            value: 'Ho Chi Minh'
+        }, {
+            shortName: 'L',
+            value: 'DAI HOC KHOA HOC VA XA HOI NHAN VAN'
+        }, {
+            shortName: 'CN',
+            value: `${shcc}-${id}`
+        }];
+
+        const extensions = [{
+            name: 'basicConstraints',
+            cA: false
+        }, {
+            name: 'nsCertType',
+            server: true
+        }, {
+            name: 'subjectKeyIdentifier'
+        }, {
+            name: 'authorityKeyIdentifier',
+            authorityCertIssuer: true,
+            serialNumber: caCert.serialNumber
+        }, {
+            name: 'keyUsage',
+            digitalSignature: true,
+            nonRepudiation: true,
+            keyEncipherment: true
+        }, {
+            name: 'extKeyUsage',
+            serverAuth: true
+        },
+            // {
+            //     name: 'subjectAltName',
+            //     altNames: validDomains.map(domain => { return { type: 2, value: domain } })
+            // }
+        ];
+
+        // Create an empty Certificate
+        let newHostCert = forge.pki.createCertificate();
+        // Set the attributes for the new Host Certificate
+        newHostCert.publicKey = hostKeys.publicKey;
+        newHostCert.serialNumber = randomSerialNumber();
+        newHostCert.validity.notBefore = getCertNotBefore();
+        newHostCert.validity.notAfter = getCertNotAfter(newHostCert.validity.notBefore);
+        newHostCert.setSubject(attributes);
+        newHostCert.setIssuer(caCert.subject.attributes);
+        newHostCert.setExtensions(extensions);
+
+        // Sign the new Host Certificate using the CA
+        newHostCert.sign(caKey, forge.md.sha512.create());
+
+        let p12Asn1 = forge.pkcs12.toPkcs12Asn1(
+            hostKeys.privateKey, newHostCert, passphrase,
+            { algorithm: '3des' });
+
+        // base64-encode p12
+        let p12Der = forge.asn1.toDer(p12Asn1).getBytes();
+        let p12b64 = forge.util.encode64(p12Der);
+
+        return { p12b64, publicKey: forge.pki.publicKeyToPem(hostKeys.publicKey) };
+    };
+
+    app.post('/api/hcth/khoa/user/download', app.permission.orCheck('rectors:login', 'manager:write'), async (req, res) => {
+        try {
+            const { passphrase } = req.body;
+            const shcc = req.session.user.shcc,
+                email = req.session.user.email;
+            const khoa = await app.model.hcthUserPublicKey.get({ shcc, kichHoat: 1 });
+            if (!khoa) throw 'Không tìm thấy khóa người dùng';
+            if (khoa.publicKey) throw 'Khóa đã được gửi đến người dùng';
+            const { p12b64, publicKey } = await genKey(khoa.id, shcc, passphrase);
+            const setting = await app.model.hcthSetting.getValue('email', 'emailPassword', 'debugEmail');
+            await app.email.normalSendEmail(setting.email, setting.emailPassword, app.isDebug ? setting.debugEmail : email, [], 'Khóa người dùng mới', 'Tệp tin khóa người dùng mới', 'Tệp tin khóa người dùng mới', [{
+                filename: `${shcc}-${khoa.id}.p12`,
+                content: p12b64,
+                encoding: 'base64'
+            }]);
+            await app.model.hcthUserPublicKey.update({ id: khoa.id }, { publicKey });
+            res.send({});
+        } catch (error) {
+            res.send({ error });
+        }
+    });
+
+    app.get('/api/hcth/yeu-cau-tao-khoa/page/:pageNumber/:pageSize', app.permission.check('hcthYeuCauTaoKhoa:read'), async (req, res) => {
+        try {
+            const filter = req.query.filter || {};
+            const filterData = app.stringify(filter);
+            const pageCondition = req.query.searchTerm;
+            const page = await app.model.hcthYeuCauTaoKhoa.searchPage(parseInt(req.params.pageNumber), parseInt(req.params.pageSize), pageCondition, filterData);
+            const { totalitem: totalItem, pagesize: pageSize, pagetotal: pageTotal, pagenumber: pageNumber, rows: list } = page;
+            res.send({
+                page: { totalItem, pageSize, pageTotal, pageNumber, pageCondition, list, filter }
+            });
+            // res.send({ items: [] });
+        } catch (error) {
+            res.send({ error });
+        }
+    });
+
+
     app.post('/api/hcth/yeu-cau-tao-khoa', app.permission.orCheck('manager:write', 'rectors:login'), async (req, res) => {
-        try{
+        try {
             const data = req.body;
             const shcc = req.session.user.shcc;
-            app.model.hcthYeuCauTaoKhoa.create({data})
+            const currentKey = await app.model.hcthUserPublicKey.get({ shcc, kichHoat: 1 });
+            if (currentKey) throw 'Không thể tạo yêu cầu khi có khóa đang được kích hoạt';
+            const hasRequest = await app.model.hcthYeuCauTaoKhoa.get({ shcc, trangThai: trangThaiRequest.CHO_DUYET.id });
+            if (hasRequest) throw 'Bạn đã có 1 yêu cầu đang chờ duyệt';
+            await app.model.hcthYeuCauTaoKhoa.create({ ...data, shcc, ngayTao: new Date().getTime(), trangThai: trangThaiRequest.CHO_DUYET.id });
+            res.send({});
         } catch (error) {
-            res.send({error});
+            res.send({ error });
         }
-        
-    })
-}
+    });
+
+    app.put('/api/hcth/yeu-cau-tao-khoa/trang-thai/:id', app.permission.check('hcthYeuCauTaoKhoa:write'), async (req, res) => {
+        try {
+            const id = req.params.id;
+            const request = await app.model.hcthYeuCauTaoKhoa.get({ id });
+            if (!request) throw 'Yêu cầu không tồn tại';
+            if (request.trangThai != trangThaiRequest.CHO_DUYET.id) throw 'Trạng thái yêu cầu không hợp lệ';
+            const shcc = req.session.user.shcc;
+            const trangThai = req.body.trangThai;
+            switch (trangThai) {
+                case trangThaiRequest.DA_DUYET.id:
+                    await approveRequest(request, shcc);
+                    break;
+                case trangThaiRequest.TU_CHOI.id:
+                    await refuseRequest(request, shcc);
+                    break;
+                default:
+            }
+            res.send({});
+        } catch (error) {
+            res.send({ error });
+        }
+    });
+
+    const approveRequest = async (request, shcc) => {
+        await app.model.hcthYeuCauTaoKhoa.update({ id: request.id }, { trangThai: trangThaiRequest.DA_DUYET.id, capNhatBoi: shcc, ngayCapNhat: new Date().getTime() });
+        await app.model.hcthUserPublicKey.create({ yeuCau: request.id, shcc: request.shcc, });
+        //TODO: send notification
+    };
+
+    const refuseRequest = async (request, shcc) => {
+        await app.model.hcthYeuCauTaoKhoa.update({ id: request.id }, { trangThai: trangThaiRequest.TU_CHOI.id, capNhatBoi: shcc, ngayCapNhat: new Date().getTime() });
+        //TODO: send notification
+    };
+};
