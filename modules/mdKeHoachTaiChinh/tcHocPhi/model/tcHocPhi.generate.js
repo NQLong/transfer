8aa757fdca01f8709920d719f800df3b444bc18c
LongNguyen <nqlong0709@gmail.com>
2022-08-04 15:09:15 +0700

    gen db

--- modules/mdKeHoachTaiChinh/tcHocPhi/model/tcHocPhi.generate.js
+++ modules/mdKeHoachTaiChinh/tcHocPhi/model/tcHocPhi.generate.js
@@ -307,9 +307,9 @@ module.exports = app => {
                 }));
         }),
 
-        downloadExcel: (done) => new Promise((resolve, reject) => {
-            app.database.oracle.connection.main.execute('BEGIN :ret:=tc_hoc_phi_download_excel(); END;',
-                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR } }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, (error, result) => {
+        downloadExcel: (filter, done) => new Promise((resolve, reject) => {
+            app.database.oracle.connection.main.execute('BEGIN :ret:=tc_hoc_phi_download_excel(:filter); END;',
+                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, filter }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, (error, result) => {
                     if (error) {
                         done && done(error);
                         reject(error);
