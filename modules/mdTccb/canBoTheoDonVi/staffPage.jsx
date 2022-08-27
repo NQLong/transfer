9c3ffa86cfbe1fef28f529cba561dc9f17a5c1cc
Nguyễn Minh Anh <minhanh20720@gmail.com>
2022-08-27 16:23:28 +0700

    Update code based on review

--- modules/mdTccb/canBoTheoDonVi/staffPage.jsx
+++ modules/mdTccb/canBoTheoDonVi/staffPage.jsx
@@ -118,8 +118,8 @@ class CanBoTheoDonVi extends AdminPage {
         let nhomRoles = ['ttDoanhNghiep'];
         ctsvNhapHocPermissions.write && nhomRoles.push('ctsvNhapHoc');
         congVanPermission.manage && nhomRoles.push('quanLyCongVanDen', 'hcthQuanLyCongVanDi');
-        managerPermission.write && nhomRoles.push('quanLyCongVanPhong', 'quanLyCongVanDiPhong', 'soanThaoCongVanDi', 'tccbDonViDangKyNhiemVu');
-        managerPermission.login && nhomRoles.push('quanLyDonVi');
+        managerPermission.write && nhomRoles.push('quanLyCongVanPhong', 'quanLyCongVanDiPhong', 'soanThaoCongVanDi');
+        managerPermission.login && nhomRoles.push('quanLyDonVi', 'tccbDonViDangKyNhiemVu');
         quanLyDaoTaoPermission && nhomRoles.push('quanLyDaoTao');
         daoTaoPermission && nhomRoles.push('daoTao');
 
