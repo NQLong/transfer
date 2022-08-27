96582753659ec7990ef02bfb7250a6e99fb854e3
Nguyen Quang Sang <9a2.nguyenquangsang@gmail.com>
2022-08-27 16:54:32 +0700

    deploy 1.0.228

--- asset/db/methods.main.sql
+++ asset/db/methods.main.sql
@@ -4885,61 +4885,6 @@ END;
 /
 --EndMethod--
 
-CREATE OR REPLACE FUNCTION HCTH_DANG_KY_SO_SEARCH_PAGE(
-    pageNumber IN OUT NUMBER,
-    pageSize IN OUT NUMBER,
-    searchTerm IN STRING,
-    totalItem OUT NUMBER,
-    pageTotal OUT NUMBER
-) RETURN SYS_REFCURSOR AS
-    my_cursor SYS_REFCURSOR;
-    ST        STRING(500) := '%' || lower(searchTerm) || '%';
-
-BEGIN
-    SELECT COUNT(*)
-    INTO totalItem
-    FROM HCTH_DANG_KY_SO dks
-    WHERE (
-              (
-                          ST = ''
-                      OR LOWER(dks.SO_CONG_VAN) LIKE ST
-                  )
-              );
-
-    IF pageNumber < 1 THEN
-        pageNumber := 1;
-    end if;
-
-    IF pageSize < 1 THEN
-        pageSize := 1;
-    end if;
-    pageTotal := CEIL(totalItem / pageSize);
-    pageNumber := LEAST(pageNumber, pageTotal);
-
-    OPEN my_cursor FOR
-        SELECT *
-        FROM (SELECT dks.ID          AS                       "id",
-                     dks.SO_CONG_VAN AS                       "soCongVan",
-
-                     ROW_NUMBER() over (ORDER BY dks.ID DESC) R
-
-              FROM HCTH_DANG_KY_SO dks
-              WHERE (
-                        (
-                                    ST = ''
-                                OR LOWER(dks.SO_CONG_VAN) LIKE ST
-                            )
-                        )
-             )
-        WHERE R BETWEEN (pageNumber - 1) * pageSize + 1 AND pageNumber * pageSize
-        ORDER BY 'id' DESC;
-
-    RETURN my_cursor;
-end;
-
-/
---EndMethod--
-
 CREATE OR REPLACE FUNCTION HCTH_DASHBOARD_GET_DATA(
     timeSelect IN NUMBER,
     TOTAL_VAN_BAN_DEN OUT SYS_REFCURSOR,
@@ -5547,6 +5492,199 @@ BEGIN
         ORDER BY NGAY_TAO;
     RETURN my_cursor;
 END;
+
+/
+--EndMethod--
+
+CREATE OR REPLACE PROCEDURE HCTH_SO_DANG_KY_CREATE_SO_VAN_BAN(
+--     ma IN NUMBER,
+    donViGui IN NUMBER,
+    capVanBan IN STRING,
+    loaiVanBan IN NUMBER,
+    nam IN NUMBER,
+    tuDong IN NUMBER,
+    ngayTao IN NUMBER
+)
+    IS
+    maxThuTu             NUMBER;
+    tenVietTatDonViGui   STRING(100);
+--     capVanBan            STRING(10);
+    tenVietTatLoaiVanBan STRING(10);
+    postfix              STRING(200);
+    counter              NUMBER(10);
+    isExists             NUMBER(10);
+--     laySoTuDong          NUMBER(1);
+
+BEGIN
+    commit;
+    set transaction isolation level SERIALIZABLE name 'update_so_van_ban';
+    SELECT dvg.TEN_VIET_TAT
+    INTO tenVietTatDonViGui
+    FROM DM_DON_VI dvg
+    WHERE dvg.MA = donViGui;
+
+    SELECT lcv.TEN_VIET_TAT
+    INTO tenVietTatLoaiVanBan
+    FROM DM_LOAI_CONG_VAN lcv
+    WHERE lcv.ID = loaiVanBan;
+
+    if tuDong = 1 then
+        BEGIN
+            SELECT MAX(SO_DI)
+            INTO maxThuTu
+            FROM HCTH_SO_DANG_KY
+            WHERE donViGui = DON_VI_GUI
+              AND (NGAY_TAO > NAM)
+              AND LOAI_CONG_VAN = capVanBan;
+        exception
+            when NO_DATA_FOUND then
+                maxThuTu := 0;
+        end;
+
+        if maxThuTu is null then
+            maxThuTu := 0;
+        end if;
+        maxThuTu := maxThuTu + 1;
+
+        postfix := '/';
+        IF tenVietTatLoaiVanBan IS NOT NULL THEN
+            postfix := postfix || tenVietTatLoaiVanBan || '-';
+        end if;
+
+        postfix := postfix || 'XHNV';
+        IF tenVietTatDonViGui IS NOT NULL THEN
+            postfix := postfix || '-' || tenVietTatDonViGui;
+        end if;
+
+        counter := 2000;
+        SELECT COUNT(*)
+        INTO isExists
+        FROM HCTH_SO_DANG_KY
+        WHERE donViGui = DON_VI_GUI
+          AND (NGAY_TAO > nam)
+          AND LOAI_CONG_VAN = capVanBan
+          AND SO_CONG_VAN = TO_CHAR(maxThuTu) || postfix;
+        WHILE isExists > 0
+            LOOP
+                if counter = 0 then
+                    RAISE INVALID_NUMBER;
+                end if;
+                maxThuTu := maxThuTu + 1;
+                counter := counter - 1;
+                SELECT COUNT(*)
+                INTO isExists
+                FROM HCTH_SO_DANG_KY
+                WHERE donViGui = DON_VI_GUI
+                  AND (NGAY_TAO > nam)
+                  AND LOAI_CONG_VAN = capVanBan
+                  AND SO_CONG_VAN = TO_CHAR(maxThuTu) || postfix;
+            end loop;
+
+        INSERT INTO HCTH_SO_DANG_KY
+        (SO_CONG_VAN, SO_DI, LOAI_CONG_VAN, LOAI_VAN_BAN, DON_VI_GUI, NGAY_TAO, TU_DONG)
+        values
+        (TO_CHAR(maxThuTu) || postfix, maxThuTu, capVanBan, loaiVanBan, donViGui, ngayTao, tuDong);
+--         values dks.SO_DI  = maxThuTu,
+--             dks.SO_CONG_VAN = TO_CHAR(maxThuTu) || postfix,
+--             dks.LOAI_CONG_VAN = capVanBan,
+--             dks.DON_VI_GUI = donViGui,
+--             dks.LOAI_VAN_BAN = loaiVanBan,
+--             dks.NGAY_TAO = nam
+--         where dks.ID = ma;
+
+    end if;
+    commit;
+end;
+
+/
+--EndMethod--
+
+CREATE OR REPLACE FUNCTION HCTH_SO_DANG_KY_SEARCH_PAGE(
+    pageNumber IN OUT NUMBER,
+    pageSize IN OUT NUMBER,
+    searchTerm IN STRING,
+    totalItem OUT NUMBER,
+    pageTotal OUT NUMBER
+) RETURN SYS_REFCURSOR AS
+    my_cursor SYS_REFCURSOR;
+    ST        STRING(500) := '%' || lower(searchTerm) || '%';
+
+BEGIN
+    SELECT COUNT(*)
+    INTO totalItem
+    FROM HCTH_SO_DANG_KY dks
+    WHERE (
+              (
+                          ST = ''
+                      OR LOWER(dks.SO_CONG_VAN) LIKE ST
+                  )
+              );
+
+    IF pageNumber < 1 THEN
+        pageNumber := 1;
+    end if;
+
+    IF pageSize < 1 THEN
+        pageSize := 1;
+    end if;
+    pageTotal := CEIL(totalItem / pageSize);
+    pageNumber := LEAST(pageNumber, pageTotal);
+
+    OPEN my_cursor FOR
+        SELECT *
+        FROM (SELECT dks.ID          AS                       "id",
+                     dks.SO_CONG_VAN AS                       "soCongVan",
+
+                     ROW_NUMBER() over (ORDER BY dks.ID DESC) R
+
+              FROM HCTH_SO_DANG_KY dks
+              WHERE (
+                        (
+                                    ST = ''
+                                OR LOWER(dks.SO_CONG_VAN) LIKE ST
+                            )
+                        )
+             )
+        WHERE R BETWEEN (pageNumber - 1) * pageSize + 1 AND pageNumber * pageSize
+        ORDER BY 'id' DESC;
+
+    RETURN my_cursor;
+end;
+
+/
+--EndMethod--
+
+CREATE OR REPLACE PROCEDURE HCTH_SO_DANG_KY_VALIDATE_SO_CONG_VAN(
+    soDangKy IN STRING,
+    capVanBan IN STRING,
+    donViGui IN NUMBER,
+    nam IN NUMBER
+--     ngayTao IN NUMBER
+)
+AS
+    counter NUMBER(10);
+BEGIN
+    commit;
+    set transaction isolation level SERIALIZABLE NAME 'HCTH_DANG_KY_SO_VALIDATE_SO_CONG_VAN';
+    BEGIN
+        SELECT COUNT(*)
+        INTO counter
+        FROM HCTH_SO_DANG_KY dks
+        WHERE dks.SO_CONG_VAN = soDangKy
+          AND dks.DON_VI_GUI = donViGui
+          AND dks.NGAY_TAO > nam
+          AND dks.LOAI_CONG_VAN = capVanBan;
+
+        if counter > 0 then
+            RAISE INVALID_NUMBER;
+--         ELSE
+--             INSERT INTO HCTH_DANG_KY_SO
+--             (SO_CONG_VAN, LOAI_CONG_VAN, DON_VI_GUI, NGAY_TAO, TU_DONG)
+--             VALUES
+--             (soDangKy, capVanBan, donViGui, ngayTao, 0);
+        end if;
+    end;
+end;
 
 /
 --EndMethod--
