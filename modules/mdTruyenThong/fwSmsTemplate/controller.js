c06a57513a9419b2f89df832074305172c2f7ba6
tientrantan30 <tientrantan30@gmail.com>
2022-09-07 12:31:44 +0700

    init sms & fix dt

---
+++ modules/mdTruyenThong/fwSmsTemplate/controller.js
@@ -0,0 +1,46 @@
+module.exports = app => {
+    const menu = {
+        parentMenu: app.parentMenu.truyenThong,
+        menus: {
+            6023: {
+                title: 'SMS Template',
+                link: '/user/truyen-thong/sms/template', icon: 'fa-file-text', groupIndex: 5
+            },
+        },
+    };
+
+    app.permission.add(
+        { name: 'fwSmsTemplate:read', menu },
+        'fwSmsTemplate:write', 'fwSmsTemplate:delete'
+    );
+
+    app.get('/user/truyen-thong/sms/template', app.permission.check('fwSmsTemplate:read'), app.templates.admin);
+
+    // API -------------------------------------------------------------------------------
+    app.get('/api/sms/template/page/:pageNumber/:pageSize', app.permission.check('fwSmsTemplate:read'), async (req, res) => {
+        try {
+            let _pageNumber = parseInt(req.params.pageNumber),
+                _pageSize = parseInt(req.params.pageSize),
+                user = req.session.user;
+            let donVi = '';
+            if (!user.permissions.includes('fwSmsTemplate:manage')) {
+                donVi = user.staff.maDonVi;
+            }
+            let page = await app.model.fwSmsTemplate.getPage(_pageNumber, _pageSize, { donVi });
+            res.send({ page });
+        } catch (error) {
+            res.send({ error });
+        }
+    });
+
+    app.post('/api/sms/template', app.permission.check('fwSmsTemplate:write'), async (req, res) => {
+        try {
+            let user = req.session.user,
+                email = user.email;
+            const item = await app.model.fwSmsTemplate.create(Object.assign(req.body.data, { approver: email, timeApproved: Date.now() }));
+            res.send({ item });
+        } catch (error) {
+            res.send({ error });
+        }
+    });
+};
