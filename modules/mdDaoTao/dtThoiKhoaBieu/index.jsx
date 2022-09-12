2efc036437f16c9775014ffad8c97e46fb15306c
hienvv17 <hienvv17@gmail.com>
2022-09-12 14:42:20 +0700

    tra cuu phog trong TKB

--- modules/mdDaoTao/dtThoiKhoaBieu/index.jsx
+++ modules/mdDaoTao/dtThoiKhoaBieu/index.jsx
@@ -21,7 +21,12 @@ export default {
         },
         {
             path: '/user/dao-tao/thoi-khoa-bieu/auto-generate',
-            component: Loadable({ loading: Loading, loader: () => import('./GenSchedPage') })
-        }
-    ]
+
+        },
+        {
+            path: '/user/dao-tao/thoi-khoa-bieu/tra-cuu',
+            component: Loadable({ loading: Loading, loader: () => import('./searchRoom') })
+        },
+    ],
+
 };
