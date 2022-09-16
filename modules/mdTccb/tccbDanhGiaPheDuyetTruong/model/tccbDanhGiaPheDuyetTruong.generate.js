f8aaa04c20143914069cca6ccdadb4c372f89812
vietphap <51202664@hcmut.edu.vn>
2022-09-16 12:18:29 +0700

    gen hàm

--- modules/mdTccb/tccbDanhGiaPheDuyetTruong/model/tccbDanhGiaPheDuyetTruong.generate.js
+++ modules/mdTccb/tccbDanhGiaPheDuyetTruong/model/tccbDanhGiaPheDuyetTruong.generate.js
@@ -1,6 +1,6 @@
-// Table name: TCCB_DANH_GIA_PHE_DUYET_TRUONG { id, userDuyetCapTruong, approvedTruong, idPheDuyetCapDonVi, yKienTruongTccb, truongTccb }
+// Table name: TCCB_DANH_GIA_PHE_DUYET_TRUONG { id, userDuyetCapTruong, approvedTruong, idPheDuyetCapDonVi, yKienTruongTccb, truongTccb, nam }
 const keys = ['ID'];
-const obj2Db = { 'id': 'ID', 'userDuyetCapTruong': 'USER_DUYET_CAP_TRUONG', 'approvedTruong': 'APPROVED_TRUONG', 'idPheDuyetCapDonVi': 'ID_PHE_DUYET_CAP_DON_VI', 'yKienTruongTccb': 'Y_KIEN_TRUONG_TCCB', 'truongTccb': 'TRUONG_TCCB' };
+const obj2Db = { 'id': 'ID', 'userDuyetCapTruong': 'USER_DUYET_CAP_TRUONG', 'approvedTruong': 'APPROVED_TRUONG', 'idPheDuyetCapDonVi': 'ID_PHE_DUYET_CAP_DON_VI', 'yKienTruongTccb': 'Y_KIEN_TRUONG_TCCB', 'truongTccb': 'TRUONG_TCCB', 'nam': 'NAM' };
 
 module.exports = app => {
     app.model.tccbDanhGiaPheDuyetTruong = {
@@ -214,6 +214,19 @@ module.exports = app => {
                     resolve(result);
                 }
             });
+        }),
+
+        searchPage: (pagenumber, pagesize, searchterm, searchnam, done) => new Promise((resolve, reject) => {
+            app.database.oracle.connection.main.execute('BEGIN :ret:=tccb_danh_gia_phe_duyet_truong_search_page(:pagenumber, :pagesize, :searchterm, :totalitem, :pagetotal, :searchnam); END;',
+                { ret: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.CURSOR }, pagenumber: { val: pagenumber, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, pagesize: { val: pagesize, dir: app.database.oracle.BIND_INOUT, type: app.database.oracle.NUMBER }, searchterm, totalitem: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER }, pagetotal: { dir: app.database.oracle.BIND_OUT, type: app.database.oracle.NUMBER }, searchnam }, (error, result) => app.database.oracle.fetchRowsFromCursor(error, result, (error, result) => {
+                    if (error) {
+                        done && done(error);
+                        reject(error);
+                    } else {
+                        done && done(null, result);
+                        resolve(result);
+                    }
+                }));
         }),
     };
 };
