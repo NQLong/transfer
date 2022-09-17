bd0cce702310a473fe93792cf77ecedb095b1b8b
Dinh The Vinh <thevinhdinh69@gmail.com>
2022-09-17 16:32:52 +0700

    add dmDienDongBhyt

---
+++ modules/mdSinhVien/svDmDienDongBhyt/adminPage.jsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getDmDienDongBhytAll, createDmDienDongBhyt, updateDmDienDongBhyt, deleteDmDienDongBhyt, getDmDienDongBhytPage} from './redux';
+import { Link } from 'react-router-dom';
+import Pagination from 'view/component/Pagination';
+import { AdminPage, AdminModal, TableCell, renderTable, FormTextBox} from 'view/component/AdminPage';
+
+class EditModal extends AdminModal {
+    state = { active: false }
+
+    componentDidMount() {
+        $(document).ready(() => this.onShown(() => {
+            !this.ma.value() ? this.ma.focus() : this.ten.focus();
+        }));
+    }
+
+    onShow = (item) => {
+        let { ma, ten, moTa, soTien, namHoc } = item ? item : { ma: '', ten: '', moTa: '', soTien: '', namHoc: '' };
+        this.setState({ma, item});
+        this.ma.value(ma);
+        this.ten.value(ten);
+        this.moTa.value(moTa);
+        this.soTien.value(soTien);
+        this.namHoc.value(namHoc);
+    };
+
+    onSubmit = (e) => {
+        const changes = {
+            ma: this.ma.value(),
+            ten: this.ten.value(), 
+            moTa: this.moTa.value(),
+            soTien: this.soTien.value(),
+            namHoc: this.namHoc.value()
+        };
+        if (changes.ma == '') {
+            T.notify('Mã châu bị trống!', 'danger');
+            this.ma.focus();
+        } else if (changes.ten == '') {
+            T.notify('Tên châu bị trống!', 'danger');
+            this.ten.focus();
+        } else if (changes.moTa == '') {
+            T.notify('Mô tả bị trống!', 'danger');
+            this.moTa.focus();
+        } else if (changes.soTien == '') {
+            T.notify('Số tiền bị trống!', 'danger');
+            this.soTien.focus();
+        } else if (changes.namHoc == '') {
+            T.notify('Năm học bị trống!', 'danger');
+            this.namHoc.focus();
+        } else {
+            this.state.ma ? this.props.update(this.state.ma, changes, this.hide) : this.props.create(changes, this.hide);
+        }
+        e.preventDefault();
+    };
+
+    // changeKichHoat = value => this.kichHoat.value(value ? 1 : 0) || this.kichHoat.value(value);
+
+    render = () => {
+        const readOnly = this.props.readOnly;
+        return this.renderModal({
+            title: this.state.ma ? 'Cập nhật diện đóngo' : 'Tạo mới diện đóng',
+            body: <div className = 'row'>
+               <FormTextBox type='number' className='col-md-12' ref={e => this.ma = e} label='Mã diện đóng' 
+                    readOnly={this.state.ma ? true : readOnly} required />
+                <FormTextBox type='text' className='col-md-12' ref={e => this.ten = e} label='Tên diện đóng' 
+                    readOnly={readOnly} required />
+                <FormTextBox type='text' className='col-md-12' ref={e => this.moTa = e} label='Mô tả' 
+                    readOnly={readOnly} required />
+                <FormTextBox type='number' className='col-md-12' ref={e => this.soTien = e} label='Số tiền'
+                    readOnly={readOnly} required/>
+                <FormTextBox type='text' className='col-md-12' ref={e => this.namHoc = e} label='Năm học' 
+                    readOnly={readOnly} required />
+            </div>
+        });
+    }
+}
+
+class DmDienDongBhytPage extends AdminPage {
+    componentDidMount() {
+        T.ready('/user/students', () => {
+            T.onSearch = (searchText) => this.props.getDmDienDongBhytPage(undefined, undefined, searchText || '');
+            T.showSearchBox();
+            this.props.getDmDienDongBhytPage();
+        });
+    }
+
+    showModal = (e) => {
+        e.preventDefault();
+        this.modal.show();
+    };
+
+    delete = (e, item) => {
+        e.preventDefault();
+        T.confirm('Diện đóng', 'Bạn có chắc bạn muốn xóa diện đóng này?', 'warning', true, isConfirm =>
+            isConfirm && this.props.deleteDmDienDongBhyt(item.ma));
+    };
+
+    render() {
+        const currentPermissions = this.props.system && this.props.system.user && this.props.system.user.permissions ? this.props.system.user.permissions : [],
+            permission = this.getUserPermission('dmDienDongBhyt', ['read', 'write', 'delete']);
+        const { pageNumber, pageSize, pageTotal, totalItem, pageCondition, list } = this.props.dmDienDongBhyt && this.props.dmDienDongBhyt.page ?
+            this.props.dmDienDongBhyt.page : { pageNumber: 1, pageSize: 50, pageTotal: 1, totalItem: 0, list: null };
+    
+        let table = 'Không có dữ liệu!';
+        if (list && list.length > 0) {
+            table = renderTable({
+                getDataSource: () => list, stickyHead: false,
+                renderHead: () => (
+                    <tr>
+                        <th style={{ width: 'auto' ,whiteSpace: 'nowrap'}} >Mã</th>
+                        <th style={{ width: '100%' ,whiteSpace: 'nowrap'}} >Tên</th>
+                        <th style={{ width: '100%' ,whiteSpace: 'nowrap'}} >Mô tả</th>
+                        <th style={{ width: '100%' ,whiteSpace: 'nowrap'}} >Số tiền</th>
+                        <th style={{ width: '100%' ,whiteSpace: 'nowrap'}} >Năm học</th>
+                        <th style={{ width: 'auto', textAlign: 'center' , whiteSpace: 'nowrap'}} >Thao tác</th>
+                    </tr>
+                ),
+                renderRow: (item, index) => (
+                    <tr key={index}>
+                        <TableCell type='text' content={item.ma ? item.ma : 0} />
+                        <TableCell type="link" content={item.ten ? item.ten : ''} 
+                            onClick={() => this.modal.show(item)} />
+                        <TableCell type='text' content={item.moTa ? item.moTa : ''} />
+                        <TableCell type='number' content={item.soTien ? item.soTien : 0} />
+                        <TableCell type='text' content={item.namHoc ? item.namHoc : ''} />
+                        <TableCell type='buttons' content={item} permission={permission} 
+                            onEdit={() => this.modal.show(item)} onDelete={e => this.delete(e, item)} />
+                    </tr>
+                )
+            });
+        }
+
+        return this.renderPage({
+            icon: 'fa fa-list-alt',
+            title: 'Danh mục Diện đóng BHYT',
+            breadcrumb: [
+                <Link key={0} to='/user/students'>Sinh Viên</Link>,
+                'Danh mục Diện đóng BHYT'
+            ],
+            content: <>
+                <div className='tile'>{table}</div>
+                <Pagination style={{ marginLeft: '70px' }} {...{ pageNumber, pageSize, pageTotal, totalItem, pageCondition }} 
+                    getPage={this.props.getDmDienDongBhytPage} />
+                <EditModal ref={e => this.modal = e} permission={permission} 
+                    create={this.props.createDmDienDongBhyt} update={this.props.updateDmDienDongBhyt} permissions={currentPermissions} />
+            </>,
+            backRoute: '/user/students',
+            onCreate: permission && permission.write ? (e) => this.showModal(e) : null,
+        });
+    }
+}
+
+const mapStateToProps = state => ({ system: state.system, dmDienDongBhyt: state.danhMuc.dmDienDongBhyt });
+const mapActionsToProps = { getDmDienDongBhytAll, createDmDienDongBhyt, updateDmDienDongBhyt, deleteDmDienDongBhyt, getDmDienDongBhytPage};
+export default connect(mapStateToProps, mapActionsToProps)(DmDienDongBhytPage);
---
+++ modules/mdSinhVien/svDmDienDongBhyt/redux.jsx
@@ -0,0 +1,146 @@
+import T from 'view/js/common';
+
+// Reducer ------------------------------------------------------------------------------------------------------------
+const DmDienDongBhytGetAll = 'DmDienDongBhyt:GetAll';
+const DmDienDongBhytGetPage = 'DmDienDongBhyt:GetPage';
+const DmDienDongBhytUpdate = 'DmDienDongBhyt:Update';
+
+export default function dmDienDongBhytReducer(state = null, data) {
+    switch (data.type) {
+        case DmDienDongBhytGetAll:
+            return Object.assign({}, state, { items: data.items });
+        case DmDienDongBhytGetPage:
+            return Object.assign({}, state, { page: data.page });
+        case DmDienDongBhytUpdate:
+            if (state) {
+                let updatedItems = Object.assign({}, state.items),
+                    updatedPage = Object.assign({}, state.page),
+                    updatedItem = data.item;
+                if (updatedItems) {
+                    for (let i = 0, n = updatedItems.length; i < n; i++) {
+                        if (updatedItems[i].ma == updatedItem.ma) {
+                            updatedItems.splice(i, 1, updatedItem);
+                            break;
+                        }
+                    }
+                }
+                if (updatedPage) {
+                    for (let i = 0, n = updatedPage.list.length; i < n; i++) {
+                        if (updatedPage.list[i].ma == updatedItem.ma) {
+                            updatedPage.list.splice(i, 1, updatedItem);
+                            break;
+                        }
+                    }
+                }
+                return Object.assign({}, state, { items: updatedItems, page: updatedPage });
+            } else {
+                return null;
+            }
+
+        default:
+            return state;
+    }
+}
+
+// Actions ------------------------------------------------------------------------------------------------------------
+T.initPage('dmDienDongBhytPage', true);
+export function getDmDienDongBhytPage(pageNumber, pageSize, pageCondition, done) {
+    const page = T.updatePage('dmDienDongBhytPage', pageNumber, pageSize, pageCondition);
+    return dispatch => {
+        const url = `/api/students/dien-dong-bhyt/page/${page.pageNumber}/${page.pageSize}`;
+        T.get(url, { condition: page.pageCondition }, data => {
+            if (data.error) {
+                T.notify('Lấy danh sách diện đóng bị lỗi ' + (data.error.message && (':<br>' + data.error.message)), 'danger');
+                console.error(`GET: ${url}.`, data.error);
+            } else {
+                if (page.pageCondition) data.page.pageCondition = page.pageCondition;
+                if (done) done(data.page);
+                dispatch({ type: DmDienDongBhytGetPage, page: data.page });
+            }
+        }, (error) => T.notify('Lấy danh sách diện đóng bị lỗi ' + (error.error.message && (':<br>' + error.error.message)), 'danger'));
+    };
+}
+
+export function getDmDienDongBhytAll(done) {
+    return dispatch => {
+        const url = '/api/students/dien-dong-bhyt/all';
+        T.get(url, data => {
+            if (data.error) {
+                T.notify('Lấy danh sách diện đóng bị lỗi' + (data.error.message && (':<br>' + data.error.message)), 'danger');
+                console.error(`GET: ${url}.`, data.error);
+            } else {
+                if (done) done(data.items);
+                dispatch({ type: DmDienDongBhytGetAll, items: data.items ? data.items : [] });
+            }
+        }, (error) => T.notify('Lấy danh sách diện đóng bị lỗi' + (error.error.message && (':<br>' + error.error.message)), 'danger'));
+    };
+}
+
+export function getDmDienDongBhyt(ma, done) {
+    return () => {
+        const url = `/api/students/dien-dong-bhyt/item/${ma}`;
+        T.get(url, data => {
+            if (data.error) {
+                T.notify('Lấy thông tin diện đóng bị lỗi' + (data.error.message && (':<br>' + data.error.message)), 'danger');
+                console.error(`GET: ${url}.`, data.error);
+            } else {
+                if (done) done(data.item);
+            }
+        }, error => console.error(`GET: ${url}.`, error));
+    };
+}
+
+export function createDmDienDongBhyt(dmDienDongBhyt, done) {
+    return dispatch => {
+        const url = '/api/students/dien-dong-bhyt';
+        T.post(url, { dmDienDongBhyt }, data => {
+            if (data.error) {
+                T.notify(data.error.message ? data.error.message : 'Tạo mới một diện đóng bị lỗi!', 'danger');
+                console.error(`POST: ${url}.`, data.error);
+            } else {
+                T.notify('Tạo mới một diện đóng thành công!', 'success');
+                dispatch(getDmDienDongBhytPage());
+                if (done) done(data);
+            }
+        }, (error) => T.notify('Tạo mới một diện đóng bị lỗi' + (error.error.message && (':<br>' + error.error.message)), 'danger'));
+    };
+}
+
+export function updateDmDienDongBhyt(ma, changes, done) {
+    return dispatch => {
+        const url = '/api/students/dien-dong-bhyt';
+        T.put(url, { ma, changes }, data => {
+            if (data.error) {
+                T.notify('Cập nhật dữ liệu diện đóng bị lỗi' + (data.error.message && (':<br>' + data.error.message)), 'danger');
+                console.error(`PUT: ${url}.`, data.error);
+            } else {
+                T.notify('Cập nhật dữ liệu diện đóng thành công!', 'success');
+                done && done(data.item);
+                dispatch(getDmDienDongBhytPage());
+            }
+        }, (error) => T.notify('Cập nhật dữ liệu diện đóng bị lỗi' + (error.error.message && (':<br>' + error.error.message)), 'danger'));
+    };
+}
+
+export function deleteDmDienDongBhyt(ma, done) {
+    return dispatch => {
+        const url = '/api/students/dien-dong-bhyt';
+        T.delete(url, { ma }, data => {
+            if (data.error) {
+                T.notify('Xóa diện đóng bị lỗi' + (data.error.message && (':<br>' + data.error.message)), 'danger');
+                console.error(`DELETE: ${url}.`, data.error);
+            } else {
+                T.alert('Xóa diện đóng thành công!', 'success', false, 800);
+                dispatch(getDmDienDongBhytPage());
+            }
+            done && done();
+        }, () => T.notify('Xóa diện đóng bị lỗi!', 'danger'));
+    };
+}
+
+export const SelectAdapter_dmDienDongBhyt = {
+    ajax: false,
+    getAll: getDmDienDongBhytAll,
+    processResults: response => ({ results: response ? response.map(item => ({ value: item.ma, text: item.ten })) : [] }),
+    condition: { kichHoat: 1 },
+};
