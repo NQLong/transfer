ce2f253e4153dfd80e5087c26b1ba9a9a2fb9d0b
tientrantan30 <tientrantan30@gmail.com>
2022-09-26 16:03:39 +0700

    download ảnh thẻ

--- config/lib/fs.js
+++ config/lib/fs.js
@@ -66,4 +66,27 @@ module.exports = app => {
             done && done(error);
         }
     };
+
+    const archiver = require('archiver');
+
+    /**
+     * @param {String} sourceDir: /some/folder/to/compress
+     * @param {String} outPath: /path/to/created.zip
+     * @returns {Promise}
+     */
+    app.fs.zipDirectory = (sourceDir, outPath) => {
+        const archive = archiver('zip', { zlib: { level: 9 } });
+        const stream = app.fs.createWriteStream(outPath);
+
+        return new Promise((resolve, reject) => {
+            archive
+                .directory(sourceDir, false)
+                .on('error', err => reject(err))
+                .pipe(stream)
+                ;
+
+            stream.on('close', () => resolve());
+            archive.finalize();
+        });
+    };
 };
