970cc2548922b59cb77607947a3a3c27255dd878
nguyen the duy <duyquangsr@gmail.com>
2022-09-27 16:28:44 +0700

    fix select_adapter

--- modules/mdDanhMuc/dmCoSoKcbBhyt/redux.jsx
+++ modules/mdDanhMuc/dmCoSoKcbBhyt/redux.jsx
@@ -135,10 +135,11 @@ export function changeDmCoSoKcb(item) {
     return { type: DmCoSoKcbUpdate, item };
 }
 
-export const SelectAdapter_DmCoSoKcbBhyt = {
+export const SelectAdapter_DmCoSoKcbBhyt = (type) => ({
     ajax: true,
     url: '/api/danh-muc/co-so-kcb-bhyt/get-all-for-adapter',
-    data: (params) => ({ searchTerm: params.term }),
-    processResults: (response) => ({ results: response && response.items ? response.items.map((item) => ({ id: item.ma, text: `[${item.ma}] ${item.ten}: ${item.diaChi}`, ten: item.ten, loaiDangKy: item.loaiDangKy })) : [] }),
-    fetchOne: (ma, done) => getDmCoSoKcb(ma, (item) => item && done && done({ id: item.ma, text: `[${item.ma}] ${item.ten}: ${item.diaChi}` }))(),
-};
+    data: (params) => ({ searchTerm: params.term, type }),
+    processResults: response => ({ results: response && response.items ? response.items.map(item => ({ id: item.ma, text: `[${item.ma}] ${item.ten}: ${item.diaChi}`, ten: item.ten, loaiDangKy: item.loaiDangKy })) : [] }),
+    fetchOne: (ma, done) => (getDmCoSoKcbBhyt(ma, item => item && done && done({ id: item.ma, text: `[${item.ma}] ${item.ten}: ${item.diaChi}` })))(),
+});
+
